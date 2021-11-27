e6d5425898361ec4794e35aa13b667d30bfa6414
Davíd Roggér <davidrogger.a@gmail.com>
2021-11-27 09:45:19 -0300

    Bloco 4.1 - Exercicio 6

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JS-primeiros-passos/dia-01-JS-exercicios/Exercicio-03-numero-maior.js
@@ -0,0 +1,13 @@
+const num1 = 10;
+const num2 = 15;
+const num3 = 20;
+
+if (num1 > num2){
+    console.log("Primeiro número é maior");
+}
+else if (num2 > num3){
+    console.log("Segundo número é maior");
+}
+else {
+    console.log("Terceiro número é maior")
+}
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JS-primeiros-passos/dia-01-JS-exercicios/Exercicio-04-positive.js
@@ -0,0 +1,15 @@
+const num1 = 10;
+
+switch (num1) {
+  case "10":
+  console.log("Positve");
+  break;
+  
+  case "20":
+  console.log("Negative")
+  break;
+
+  default:
+    console.log("zero")
+
+}
