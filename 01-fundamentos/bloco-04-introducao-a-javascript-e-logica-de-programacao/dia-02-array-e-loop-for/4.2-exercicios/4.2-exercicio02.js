5f392cf551a85dc42caa215a6b3e76752edf426a
Davíd Roggér <davidrogger.a@gmail.com>
2021-11-28 11:22:52 -0300

    Bloco 4.2 - Exercicio 02

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/4.2-exercicios/4.2-exercicio02.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/4.2-exercicios/4.2-exercicio02.js
@@ -1,7 +1,8 @@
-// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-for (let todoArray of numbers) {
-  console.log(todoArray)
+let soma = 0;
+for (let contador = 0; contador < numbers.length; contador += 1){
+  soma += numbers[contador]
 }
+console.log(`Soma de todo conteúdo do Arrays é ${soma}`);
