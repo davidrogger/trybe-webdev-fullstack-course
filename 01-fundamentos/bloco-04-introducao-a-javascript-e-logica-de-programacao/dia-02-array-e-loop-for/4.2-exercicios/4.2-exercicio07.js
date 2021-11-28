a47f55843cc76736e220efe5ea309ebc05da1128
Davíd Roggér <davidrogger.a@gmail.com>
2021-11-28 11:52:45 -0300

    Bloco 4.2 - Exercicio 07

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/4.2-exercicios/4.2-exercicio07.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/4.2-exercicios/4.2-exercicio07.js
@@ -1,14 +1,13 @@
-// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+// Utilizando for , descubra qual o menor valor contido no array e imprima-o
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let oddNumber = 0;
+let minorNumber;
 for (let contador = 0; contador < numbers.length; contador += 1){
-  if (numbers[contador] % 2 !== 0){
-    oddNumber += 1;
+  if(typeof(minorNumber) !== "number"){
+    minorNumber = numbers[contador];
+  }
+  if (numbers[contador] < minorNumber){
+    minorNumber = numbers[contador]
   }
 }
-if (oddNumber > 0) {
-  console.log(`Existem ${oddNumber} números ímpares neste array.`)
-} else {
-  console.log("Nenhum valor ímpar encontrado")
-}
+console.log("Menor número dentro do array é "+ minorNumber);
