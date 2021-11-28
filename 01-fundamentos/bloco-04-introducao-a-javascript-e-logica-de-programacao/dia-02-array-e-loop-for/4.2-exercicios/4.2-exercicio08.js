d6ebe13e02ad573dd5d45cbfce077cdaacba85b4
Davíd Roggér <davidrogger.a@gmail.com>
2021-11-28 11:56:24 -0300

    Bloco 4.2 - Exercicio 08

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/4.2-exercicios/4.2-exercicio08.js
@@ -0,0 +1,11 @@
+// Utilizando for , descubra qual o menor valor contido no array e imprima-o
+
+let numbers = [];
+for (let contador = 1; contador <= 25; contador += 1){
+  numbers.push(contador);
+}
+// for (let resultado of numbers) {
+//   console.log(resultado);
+// }
+// ou
+console.log(numbers)
