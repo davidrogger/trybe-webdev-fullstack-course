d520fb9e2859f6488343dbcab5cdf785e79eadc1
Davíd Roggér <davidrogger.a@gmail.com>
2021-11-28 10:38:05 -0300

    Bloco 4.2 - Para fixar - exercicio02

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/4.2-exercicios/4.2-para-fixar-exercicio01 copy.js
@@ -0,0 +1,5 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let findItem = menu.indexOf("Serviços");
+let menuServices = menu[findItem];
+
+console.log(menuServices);
--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/4.2-exercicios/4.2-para-fixar-exercicio02.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/4.2-exercicios/4.2-para-fixar-exercicio02.js
@@ -1,4 +1,4 @@
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = ;
+let indexOfPortfolio = ;
 
-console.log(menuServices);
+console.log(indexOfPortfolio);
