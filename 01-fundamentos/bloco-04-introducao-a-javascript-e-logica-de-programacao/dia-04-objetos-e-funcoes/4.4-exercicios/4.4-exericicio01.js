f361e600eb959dba4fc69a3086cb58a3bb4aacf4
Davíd Roggér <davidrogger.a@gmail.com>
2021-11-30 18:21:34 -0300

    Passo 02 exercicio 01

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicios/4.4-exericicio01.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicios/4.4-exericicio01.js
@@ -3,4 +3,7 @@ let info = {
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos, Pato Donald'
 };
-console.log(`Bem vinda, ${info.personagem}`);
+info['recorrente'] = 'Sim';
+
+console.log(info)
+// console.log(`Bem vinda, ${info.personagem}`); passo 01
