6c5e5a90fc519b4eda0624ef72e1ccac499f6059
Davíd Roggér <davidrogger.a@gmail.com>
2021-12-01 08:03:00 -0300

    exercicio02

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicios/4.4-exercicio02.js
@@ -0,0 +1,11 @@
+let leitor = {
+  nome: 'Julia',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O pior Dia de todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+}
