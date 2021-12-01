a1a99bfda77be1842685bb17769546761e27006c
Davíd Roggér <davidrogger.a@gmail.com>
2021-12-01 08:09:03 -0300

    Passo 01-6 exercicio02

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicios/4.4-exercicio02.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicios/4.4-exercicio02.js
@@ -1,5 +1,6 @@
 let leitor = {
   nome: 'Julia',
+  sobrenome: 'Pessoa',
   idade: 21,
   livrosFavoritos: [
     {
@@ -9,3 +10,7 @@ let leitor = {
     },
   ],
 }
+
+
+// Passo 01 - 06
+console.log(`O livro favorito da ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos['0']['titulo']}.`)
