7a1d2155e425a635c73b0b8f9c37d463dd2ef945
Davíd Roggér <davidrogger.a@gmail.com>
2021-12-01 08:13:49 -0300

    Inicio do passo 02 - 07

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicios/4.4-exercicio02.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicios/4.4-exercicio02.js
@@ -10,7 +10,8 @@ let leitor = {
     },
   ],
 }
-
+// Passo 02 - 07
+leitor.livrosFavoritos.push(`{titulo: 'Harry Potter e o Prisioneiro de Azkaban', autor: 'JK Rowling', editor: 'Rocco',}`)
 
 // Passo 01 - 06
-console.log(`O livro favorito da ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos['0']['titulo']}.`)
+// console.log(`O livro favorito da ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos['0']['titulo']}.`)
