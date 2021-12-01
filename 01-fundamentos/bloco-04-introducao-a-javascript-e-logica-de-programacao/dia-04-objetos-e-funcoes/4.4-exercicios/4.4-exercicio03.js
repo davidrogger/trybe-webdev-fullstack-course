c4040228d1a4d2980d060a2a314f24825767ca1f
Davíd Roggér <davidrogger.a@gmail.com>
2021-12-01 09:42:31 -0300

    Adicionado enunciado - exercicio parte 2 - passo 01

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicios/4.4-exercicio03.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/4.4-exercicios/4.4-exercicio03.js
@@ -1,3 +1,10 @@
+// Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+// Exemplo de palíndromo: arara .
+// verificaPalindrome('arara') ;
+// Retorno esperado: true
+// verificaPalindrome('desenvolvimento') ;
+// Retorno esperado: false
+
 const testName = 'desenvolvimento';
 
 function checkName(){  
