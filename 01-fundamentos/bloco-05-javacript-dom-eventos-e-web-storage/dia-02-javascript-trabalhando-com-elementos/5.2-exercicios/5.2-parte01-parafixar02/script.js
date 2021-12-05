509ce9179c278180cdee24dbe1939d92b8d8304e
Davíd Roggér <davidrogger.a@gmail.com>
2021-12-05 10:03:33 -0300

    Exercicios para fixar busca de elementos + inicio de create element

---
+++ 01-fundamentos/bloco-05-javacript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/5.2-exercicios/5.2-parte01-parafixar02/script.js
@@ -0,0 +1,16 @@
+// Acesse o elemento elementoOndeVoceEsta .
+let ondeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+ondeVoceEsta.parentNode.style.color = 'green';
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Primeiro Filho do Filho';
+// Acesse o primeiroFilho a partir de pai .
+document.getElementById('pai').firstElementChild;
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+document.getElementById('elementoOndeVoceEsta').parentNode.firstElementChild;
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+document.getElementById('elementoOndeVoceEsta').nextSibling;
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+document.getElementById('elementoOndeVoceEsta').nextElementSibling;
+// Agora acesse o terceiroFilho a partir de pai .
+document.getElementById('pai').lastElementChild.previousElementSibling;
