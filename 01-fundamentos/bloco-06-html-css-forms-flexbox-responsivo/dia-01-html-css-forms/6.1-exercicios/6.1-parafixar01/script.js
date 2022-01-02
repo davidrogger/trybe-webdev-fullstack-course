f892c8d46ac478c7cb722c641fb45e9dbd628567
Davíd Roggér <davidrogger.a@gmail.com>
2022-01-02 10:02:44 -0300

    para fixar 01, concluido

--- 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/6.1-exercicios/6.1-parafixar01/script.js
+++ 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/6.1-exercicios/6.1-parafixar01/script.js
@@ -3,3 +3,20 @@ const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
 
+function href_none(event) {
+  event.preventDefault();
+}
+
+function checkbox_none(event) {
+  event.preventDefault();
+}
+
+function inputText_none(event) {
+  if (event.key !== 'a') {
+    event.preventDefault();
+  }
+}
+
+HREF_LINK.addEventListener('click', href_none);
+INPUT_CHECKBOX.addEventListener('click', checkbox_none);
+INPUT_TEXT.addEventListener('keypress', inputText_none);
