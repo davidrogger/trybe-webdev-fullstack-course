41c005986978b3c90fcf51c81b9c6f79772d7ed9
Davíd Roggér <davidrogger.a@gmail.com>
2022-01-03 09:39:06 -0300

    inicio da funcao de submit

--- 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/6.1-exercicios/6.1-exercicio-do-dia/script.js
+++ 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/6.1-exercicios/6.1-exercicio-do-dia/script.js
@@ -1,6 +1,7 @@
 // Seletores
 const selectStates = document.getElementById('select-states');
 const dataStarts = document.getElementById('input-data-starts');
+const inputSubmit = document.getElementById('input-submit');
 
 // https://mundoeducacao.uol.com.br/geografia/estados-brasil.htm
 const allStates = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
@@ -19,5 +20,10 @@ function dataStartsFilter(event) {
   }  
 }
 
-dataStarts.addEventListener('keypress', dataStartsFilter);
+function customeSubmit(event) {
+  event.preventDefault();
+}
+
 allStates.forEach(createState);
+dataStarts.addEventListener('keypress', dataStartsFilter);
+inputSubmit.addEventListener('click', customeSubmit)
