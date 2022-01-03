896a25a4cff5e3ea8d69fd7b3d2f9f2f6953fe5e
Davíd Roggér <davidrogger.a@gmail.com>
2022-01-03 09:13:46 -0300

    criacao do header com titulo

--- 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/6.1-exercicios/6.1-exercicio-do-dia/script.js
+++ 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/6.1-exercicios/6.1-exercicio-do-dia/script.js
@@ -1,5 +1,7 @@
 // Seletores
 const selectStates = document.getElementById('select-states');
+const dataStarts = document.getElementById('input-data-starts');
+
 // https://mundoeducacao.uol.com.br/geografia/estados-brasil.htm
 const allStates = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
 
@@ -9,4 +11,13 @@ function createState(indexValue) {
     selectStates.appendChild(state);
 }
 
+function dataStartsFilter(event) {  
+  if (event.key === 'Enter') {
+    const startsValue = dataStarts.value;
+    
+    console.log(startsValue);
+  }  
+}
+
+dataStarts.addEventListener('keypress', dataStartsFilter);
 allStates.forEach(createState);
