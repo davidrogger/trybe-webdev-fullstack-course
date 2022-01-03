5395709a4d13cba381f24cee3d7e49dd5dde704c
Davíd Roggér <davidrogger.a@gmail.com>
2022-01-03 11:24:03 -0300

    funcionalidade de limpar todos os valores preenchidos no formulario

--- 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/6.1-exercicios/6.1-exercicio-do-dia/script.js
+++ 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/6.1-exercicios/6.1-exercicio-do-dia/script.js
@@ -1,8 +1,12 @@
 // Seletores
 const selectStates = document.getElementById('select-states');
-const dataStarts = document.getElementById('input-data-starts');
 const inputSubmit = document.getElementById('input-submit');
 const inputClean = document.getElementById('input-clean');
+const displayConclusion = document.getElementById('form-conclusion');
+// Forms
+const inputName = document.getElementById('input-name');
+
+const dataStarts = document.getElementById('input-data-starts');
 
 // https://mundoeducacao.uol.com.br/geografia/estados-brasil.htm
 const allStates = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
@@ -21,12 +25,26 @@ function dataStartsFilter(event) {
   }  
 }
 
-function customSubmit(event) {
-  event.preventDefault();
+function checkName(name) {  
+  if (name.length === 0) {
+    
+  }
 }
 
-function cleanForms() {
+function customSubmit(event) {
+  event.preventDefault();  
+  checkName(inputName.value);
+}
 
+// Esvazia os valores de cada campo coletado na funcao de cleanForms.
+function cleanInputText(index) {
+  index.value = '';
+}
+
+// Coleta todos classes de input do usuário.
+function cleanForms() {
+  const allInputText = document.querySelectorAll('.input-user');
+  allInputText.forEach(cleanInputText);
 }
 
 function fullLoad() {
