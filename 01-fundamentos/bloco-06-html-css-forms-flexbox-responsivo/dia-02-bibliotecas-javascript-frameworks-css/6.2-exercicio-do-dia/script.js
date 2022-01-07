67c9df93162580b558384ac726fae92607631f95
Davíd Roggér <davidrogger.a@gmail.com>
2022-01-07 10:43:48 -0300

    removendo validacoes e eventos manuais

--- 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-02-bibliotecas-javascript-frameworks-css/6.2-exercicio-do-dia/script.js
+++ 01-fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-02-bibliotecas-javascript-frameworks-css/6.2-exercicio-do-dia/script.js
@@ -1,17 +1,13 @@
 // Seletores
 const selectStates = document.getElementById('select-states');
 const inputSubmit = document.getElementById('input-submit');
-const inputClean = document.getElementById('input-clean');
 const displayConclusion = document.getElementById('form-conclusion');
 // Forms
 const inputName = document.getElementById('input-name');
 const inputEmail = document.getElementById('input-email');
 const inputState = document.getElementById('select-states');
 let inputRadioHouse = document.querySelector('[name="radio-type"]:checked');
-let picker = new Pikaday({ field: document.getElementById('datepicker'), format: 'dd/mm/yyyy' });
-// const dataStarts = document.getElementById('input-data-starts');
-// const dateInvalid = document.getElementById('date-invalid');
-// const dataInvalid = document.getElementById('data-invalid')
+
 
 // https://mundoeducacao.uol.com.br/geografia/estados-brasil.htm
 const allStates = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
@@ -29,27 +25,6 @@ function createState(indexValue) {
     selectStates.appendChild(state);
 }
 
-// Verifica a quantidade de caracteres preenchidos para validar se algo preenchido no determinado campo.
-function minimalLength() {
-  const allInputText = refreshInputUser();  
-  for (let index = 0; index < allInputText.length; index += 1) {
-    let indexTrim = allInputText[index].value.trim();
-    if (indexTrim.length === 0) {
-      return false;
-    }  
-  } 
-  return true;
-}
-
-// Verificar se foi selecionado algum dos botoes de radio para validar.
-function radioCheck() {
-  const radioValidation = document.querySelector('[name="radio-type"]:checked');
-  if (radioValidation === null) {
-    return false;
-  }
-  return true;
-}
-
 // Criar elementos paragrafo no container de conclusão do forms
 function displayForm(index) {
   const displayContainer = document.createElement('p');
@@ -62,52 +37,6 @@ function displayForm(index) {
     displayContainer.innerText = index.value;
     displayConclusion.appendChild(displayContainer);
   }
-}
-// remove o checked de todos radios buttons que pertencem ao nome radio-type
-function cleanRadioButton() {
-  const allRadioTypeHouse = document.getElementsByName('radio-type');
-  for (let index = 0; index < allRadioTypeHouse.length; index += 1) {
-    allRadioTypeHouse[index].checked = false;
-  }
-}
-
-// Esvazia os valores de cada campo coletado na funcao de cleanForms.
-function cleanInputText(index) {
-  index.value = '';
-}
-
-// Verificar se o preenchimento do campo de datas está conforme o parametro determinado
-// function dateFormat() {
-//   const correctDate = /^\d\d\/\d\d\/\d\d\d\d$/;
-//   const inputDate = document.getElementById('input-data-starts').value;
-//   if (correctDate.test(inputDate)) {
-//     return true;
-//   }  
-//   return false;
-// }
-
-// Divido o formato de datas em 3 para verificar se o dia, mês e ano, estão dentro do requerido.
-// function dateValidNumbers() {
-//   const inputDate = document.getElementById('input-data-starts').value;
-//   const inputDateSplit = inputDate.split('/');
-//   const inputDay = parseInt(inputDateSplit[0]);
-//   const inputMonth = parseInt(inputDateSplit[1]);
-//   const inputYear = parseInt(inputDateSplit[2]);
-//   if (inputDay <= 0 && inputDay > 31) {
-//     return false;
-//   }
-//   if (inputMonth <= 0 && inputMonth > 12) {
-//     return false;
-//   }
-//   if (inputYear <= 0) {
-//     return false;
-//   }
-//   return true;
-// }
-
-function dateValidation() {
-  const dataInput = document.getElementById('input-data-starts').DataPickerX.init();
-  return dataInput;
 }
 
 // Coleta todos classes de input do usuário.
@@ -129,23 +58,19 @@ function validationHandler() {
     dataInvalid.innerText = '';
     const allInputText = refreshInputUser();
     allInputText.forEach(displayForm);    
-  } else { 
-    displayConclusion.innerText = '';   
-    dataInvalid.innerText = 'Por favor revise se todos campos foram preenchidos corretamente';    
-  }
+  } 
 }
 
 // Ativa funcionalidades ao clicar em enviar
 function customSubmit(event) {
   event.preventDefault();
-  validationHandler();  
+  // validationHandler();  
 }
 
 // Executa as funções depois que a página carregar por completo
 function fullLoad() {
   allStates.forEach(createState);  
-  inputSubmit.addEventListener('click', customSubmit);
-  inputClean.addEventListener('click', cleanForms);
+  inputSubmit.addEventListener('click', customSubmit);  
 }
 
 window.onload = fullLoad();
