738eec7868eab166fce14697c8b106cc359a393e
Davíd Roggér <davidrogger.a@gmail.com>
2022-01-18 16:00:04 -0300

    8.2 readme update + parafixar

--- 01-fundamentos/bloco-08-higher-order-funcstions-do-java-script-es6/README.md
+++ 01-fundamentos/bloco-08-higher-order-funcstions-do-java-script-es6/README.md
@@ -17,7 +17,7 @@ São funções que recebem outra função como parametro ou retornam uma funçã
 
 # HOFs que são utilizadas com arrays
 
-## [forEach](https://www.w3schools.com/jsref/jsref_foreach.asp);
+## [array.forEach()](https://www.w3schools.com/jsref/jsref_foreach.asp);
 
 Syntax
 `array.forEach((itemValue, index, array) => {})`
@@ -33,4 +33,19 @@ Nesse forEach, eu indiquei que o array era o names, para cada array, ele irá mo
 
 Existe também a possibilidade de apenas chamar a função, sem apontar o itemValue, por padrão ele vai considerar o parametro na função chamada como o valor dentro do array.
 Somente o ultimo parametro do forEach é opcional na maioria das execuções, ele mostra todo valor do array, que está sendo tratado, que nesse caso, seria `['David', 'Jonas', 'Nizuk']`.
+
+## [array.find()](https://www.w3schools.com/jsref/jsref_find.asp)
+
+Syntax
+`array.find((itemValue, index, array) => {})`
+
+Segue o mesmo padrão do forEach, porém só retorna o valor do array, caso a seja verdadeira a confição passada dentro da função callback apontada no find.
+
+Exemplo
+```
+const arrayNumbers = [10, 20, 30, 50];
+
+arrayNumbers.find((itemValue) => itemValue > 25); // retorna 30
+```
+
 
--- 01-fundamentos/bloco-08-higher-order-funcstions-do-java-script-es6/dia-02-higher-order-functions-foreach-find-some-every-sort/8.2-Exercicios/8.2-parafixar01.js
+++ 01-fundamentos/bloco-08-higher-order-funcstions-do-java-script-es6/dia-02-higher-order-functions-foreach-find-some-every-sort/8.2-Exercicios/8.2-parafixar01.js
@@ -1,12 +1,48 @@
-const emailListInData = [
-  'roberta@email.com',
-  'paulo@email.com',
-  'anaroberta@email.com',
-  'fabiano@email.com',
-];
+// forEach
+// const emailListInData = [
+//   'roberta@email.com',
+//   'paulo@email.com',
+//   'anaroberta@email.com',
+//   'fabiano@email.com',
+// ];
 
-const showEmailList = (email) => {
-  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+// const showEmailList = (email) => {
+//   console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+// };
+
+// emailListInData.forEach(showEmailList);
+
+//find
+//1- Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const findDivisibleBy3And5 = (arrayNumbers) => {
+//   return arrayNumbers.find((itemValue) => itemValue % 5 === 0 && itemValue % 3 === 0);
+// }
+
+// console.log(findDivisibleBy3And5(numbers))
+
+//2- Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// const findNameWithFiveLetters = (arrayNames) => {
+//   return arrayNames.find((itemValue) => itemValue.length === 5);
+// }
+
+// // console.log(names[0].length)
+// console.log(findNameWithFiveLetters(names));
+
+//3- Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  return musicas.find((itemValue) => itemValue.id === id);
 };
 
-emailListInData.forEach(showEmailList);
+console.log(findMusic('31031685'))
