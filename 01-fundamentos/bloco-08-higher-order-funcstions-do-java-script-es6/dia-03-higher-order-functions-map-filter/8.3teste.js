590d73c4900e112e39958118c917868da2b1a9d5
Davíd Roggér <davidrogger.a@gmail.com>
2022-01-19 19:25:43 -0300

    8.3 exercise7

--- 01-fundamentos/bloco-08-higher-order-funcstions-do-java-script-es6/dia-03-higher-order-functions-map-filter/8.3teste.js
+++ 01-fundamentos/bloco-08-higher-order-funcstions-do-java-script-es6/dia-03-higher-order-functions-map-filter/8.3teste.js
@@ -22,94 +22,94 @@
 // const listProducts = updateProducts(products, prices);
 // console.log(listProducts);
 
-const estudantes = [
-  {
-    nome: 'Jorge',
-    sobrenome: 'Silva',
-    idade: 14,
-    turno: 'Manhã',
-    materias: [
-      { name: 'Matemática', nota: 67 },
-      { name: 'Português', nota: 79 },
-      { name: 'Química', nota: 70 },
-      { name: 'Biologia', nota: 65 },
-    ],
-  },
-  {
-    nome: 'Mario',
-    sobrenome: 'Ferreira',
-    idade: 15,
-    turno: 'Tarde',
-    materias: [
-      { name: 'Matemática', nota: '59' },
-      { name: 'Português', nota: '80' },
-      { name: 'Química', nota: '78' },
-      { name: 'Biologia', nota: '92' },
-    ],
-  },
-  {
-    nome: 'Jorge',
-    sobrenome: 'Santos',
-    idade: 15,
-    turno: 'Manhã',
-    materias: [
-      { name: 'Matemática', nota: '76' },
-      { name: 'Português', nota: '90' },
-      { name: 'Química', nota: '70' },
-      { name: 'Biologia', nota: '80' },
-    ],
-  },
-  {
-    nome: 'Maria',
-    sobrenome: 'Silveira',
-    idade: 14,
-    turno: 'Manhã',
-    materias: [
-      { name: 'Matemática', nota: '91' },
-      { name: 'Português', nota: '85' },
-      { name: 'Química', nota: '92' },
-      { name: 'Biologia', nota: '90' },
-    ],
-  },
-  {
-    nome: 'Natalia',
-    sobrenome: 'Castro',
-    idade: 14,
-    turno: 'Manhã',
-    materias: [
-      { name: 'Matemática', nota: '70' },
-      { name: 'Português', nota: '70' },
-      { name: 'Química', nota: '60' },
-      { name: 'Biologia', nota: '50' },
-    ],
-  },
-  {
-    nome: 'Wilson',
-    sobrenome: 'Martins',
-    idade: 14,
-    turno: 'Manhã',
-    materias: [
-      { name: 'Matemática', nota: '80' },
-      { name: 'Português', nota: '82' },
-      { name: 'Química', nota: '79' },
-      { name: 'Biologia', nota: '75' },
-    ],
-  },
-];
+// const estudantes = [
+//   {
+//     nome: 'Jorge',
+//     sobrenome: 'Silva',
+//     idade: 14,
+//     turno: 'Manhã',
+//     materias: [
+//       { name: 'Matemática', nota: 67 },
+//       { name: 'Português', nota: 79 },
+//       { name: 'Química', nota: 70 },
+//       { name: 'Biologia', nota: 65 },
+//     ],
+//   },
+//   {
+//     nome: 'Mario',
+//     sobrenome: 'Ferreira',
+//     idade: 15,
+//     turno: 'Tarde',
+//     materias: [
+//       { name: 'Matemática', nota: '59' },
+//       { name: 'Português', nota: '80' },
+//       { name: 'Química', nota: '78' },
+//       { name: 'Biologia', nota: '92' },
+//     ],
+//   },
+//   {
+//     nome: 'Jorge',
+//     sobrenome: 'Santos',
+//     idade: 15,
+//     turno: 'Manhã',
+//     materias: [
+//       { name: 'Matemática', nota: '76' },
+//       { name: 'Português', nota: '90' },
+//       { name: 'Química', nota: '70' },
+//       { name: 'Biologia', nota: '80' },
+//     ],
+//   },
+//   {
+//     nome: 'Maria',
+//     sobrenome: 'Silveira',
+//     idade: 14,
+//     turno: 'Manhã',
+//     materias: [
+//       { name: 'Matemática', nota: '91' },
+//       { name: 'Português', nota: '85' },
+//       { name: 'Química', nota: '92' },
+//       { name: 'Biologia', nota: '90' },
+//     ],
+//   },
+//   {
+//     nome: 'Natalia',
+//     sobrenome: 'Castro',
+//     idade: 14,
+//     turno: 'Manhã',
+//     materias: [
+//       { name: 'Matemática', nota: '70' },
+//       { name: 'Português', nota: '70' },
+//       { name: 'Química', nota: '60' },
+//       { name: 'Biologia', nota: '50' },
+//     ],
+//   },
+//   {
+//     nome: 'Wilson',
+//     sobrenome: 'Martins',
+//     idade: 14,
+//     turno: 'Manhã',
+//     materias: [
+//       { name: 'Matemática', nota: '80' },
+//       { name: 'Português', nota: '82' },
+//       { name: 'Química', nota: '79' },
+//       { name: 'Biologia', nota: '75' },
+//     ],
+//   },
+// ];
 
 // const morningStundants = estudantes.filter((itemValue) => itemValue.turno === 'Manhã').map((itemValue) => `${itemValue.nome} ${itemValue.sobrenome}`);
 // console.log(morningStundants);
 
 
-const materiaStudant = (studantName, array) => {
-  const studant = array.find((itemValue) => itemValue.nome === studantName);
-  return studant.materias.map((itemValue) => {
-    if(itemValue.nota >= 60) {
-      return `${itemValue.name}: ${itemValue.nota}: Aprovado`;
-    }
-    return `${itemValue.name}: ${itemValue.nota}: Reprovado.`
-  })
+// const materiaStudant = (studantName, array) => {
+//   const studant = array.find((itemValue) => itemValue.nome === studantName);
+//   return studant.materias.map((itemValue) => {
+//     if(itemValue.nota >= 60) {
+//       return `${itemValue.name}: ${itemValue.nota}: Aprovado`;
+//     }
+//     return `${itemValue.name}: ${itemValue.nota}: Reprovado.`
+//   })
   
-}
+// }
 
-console.log(materiaStudant('Natalia', estudantes));
+// console.log(materiaStudant('Natalia', estudantes));
