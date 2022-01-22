d98414d6113454edade8f8764cbe8544a9a6d7fd
Davíd Roggér <davidrogger.a@gmail.com>
2022-01-22 11:40:43 -0300

    8.5 exercises done

--- 01-fundamentos/bloco-08-higher-order-funcstions-do-java-script-es6/dia-05-spread-operator-parametro-rest-destructuring/8.5-exercises/8.5-exercise2.js
+++ 01-fundamentos/bloco-08-higher-order-funcstions-do-java-script-es6/dia-05-spread-operator-parametro-rest-destructuring/8.5-exercises/8.5-exercise2.js
@@ -1,6 +1,6 @@
+//2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+// Dica: use parâmetro rest .
+
 const sum = (...sum) => sum.reduce((total, number) => total + number, 0);
 
 console.log(sum(5, 10, 30));
-
-// O Segundo exercicio do bloco 8.5
-// Exercício bom, Colocamos em prática um exemplo até mostrado no curso,  lembrando o uso do reduce, aproveitando do parâmetro rest.
