cdb8376a48a2749554d2d430c8391e2cf9bddc67
Davíd Roggér <davidrogger.a@gmail.com>
2022-02-01 20:15:42 -0300

    atividade de fixação

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -7,11 +7,16 @@ class App extends Component {
     this.firstButton = this.firstButton.bind(this);
     this.secondButton = this.secondButton.bind(this);
     this.thirdButton = this.thirdButton.bind(this);
+    this.state = {
+      countClick: 0
+    }
+
   }
 
-  firstButton() {
-    console.log(this)
-    console.log('Ha')
+  firstButton() {    
+    this.setState((state, _props) => ({
+      countClick: state.countClick +1
+    }))
   }
   
   secondButton() {
@@ -24,7 +29,7 @@ class App extends Component {
   render() {    
     return (
       <div>
-        <button onClick={this.firstButton}>Ha</button>
+        <button onClick={this.firstButton}>{this.state.countClick}</button>
         <button onClick={this.secondButton}>du</button>
         <button onClick={this.thirdButton}>ken</button>
       </div>
@@ -33,3 +38,71 @@ class App extends Component {
 }
 
 export default App;
+
+
+// Parte 02
+// import React, { Component } from 'react'
+// import './App.css';
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.firstButton = this.firstButton.bind(this);
+//     this.secondButton = this.secondButton.bind(this);
+//     this.thirdButton = this.thirdButton.bind(this);
+//   }
+
+//   firstButton() {
+//     console.log(this)
+//     console.log('Ha')
+//   }
+  
+//   secondButton() {
+//     console.log('du')
+//   }
+//   thirdButton() {
+//     console.log('ken')    
+//   }
+
+//   render() {    
+//     return (
+//       <div>
+//         <button onClick={this.firstButton}>Ha</button>
+//         <button onClick={this.secondButton}>du</button>
+//         <button onClick={this.thirdButton}>ken</button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
+///////////// Parte 1
+// import React, { Component } from 'react'
+// import './App.css';
+
+// function firstButton() {
+//   console.log('Ha')
+// }
+
+// function secondButton() {
+//   console.log('du')
+// }
+
+// function thirdButton() {
+//   console.log('Ha')
+// }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={firstButton}>Ha</button>
+//         <button onClick={secondButton}>du</button>
+//         <button onClick={thirdButton}>ken</button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
