334f3f78229245a958b48cabaa38237618dbcc5e
Davíd Roggér <davidrogger.a@gmail.com>
2022-02-01 11:50:00 -0300

    10.2 - exercise do dia falta estilizar mais

---
+++ 02-front-end/bloco-10-introducao-front-end-react/dia-02-componentes-react/my-pokedex/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Pokedex from './pokedex';
+
+function App() {
+  return (
+    <main className='main-container'>
+      <header className='head-container'>
+        <h1>Pokedex</h1>
+      </header>
+      <Pokedex />
+    </main>
+  );
+}
+
+export default App;
