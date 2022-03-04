4b885bc60ab825c58ee102f41bb95c97d892272e
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-04 09:15:11 -0300

    atualização na altura do bloco do pokemon na pokedex

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex-2/src/App.js
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex-2/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Pokedex from './pokedex';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <main className='main-container'>
-      <header className='head-container'>
-        <h1>Pokedex</h1>
-      </header>
-      <Pokedex />
-    </main>
-  );
+class App extends Component {
+  render () {
+    return (
+      <main className='main-container'>
+        <header className='head-container'>
+          <h1>Pokedex</h1>
+        </header>
+        <Pokedex />
+      </main>
+    );
+  }
 }
 
 export default App;
