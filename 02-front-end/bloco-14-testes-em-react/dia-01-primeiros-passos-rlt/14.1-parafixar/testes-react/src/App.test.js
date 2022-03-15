e975feed36b69e125c88cb1334ac7fa79bab1112
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-15 07:41:57 -0300

    atividades para fixar conteudo do dia

--- 02-front-end/bloco-14-testes-em-react/dia-01-primeiros-passos-rlt/14.1-parafixar/testes-react/src/App.test.js
+++ 02-front-end/bloco-14-testes-em-react/dia-01-primeiros-passos-rlt/14.1-parafixar/testes-react/src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react tomorrow/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Teste', () => {
+  test('Verifica se existe um input, com label Email, e que tenha a propridade de tipo como email', () => {
+    render(<App />);
+    const inputEmail = screen.getByLabelText('Email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail).toHaveProperty('type', 'email');
+  });
+  
+})
