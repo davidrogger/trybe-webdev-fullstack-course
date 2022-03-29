46b625ac45b56f9e680b148b53223b54ef3c53b9
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-29 14:12:39 -0300

    realizando teste com renderWithRedux

--- 02-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-react-redux/15.5-parafixar/redux-tests/src/App.test.js
+++ 02-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-05-testes-react-redux/15.5-parafixar/redux-tests/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 import { createStore, combineReducers } from 'redux';
 import clickReducer from './reducers';
@@ -14,3 +15,19 @@ const renderWithRedux = (
   }
 }
 
+describe('Elements need in the aplication', () => {
+  // beforeEach(cleanup);
+  it('Should have a button "clique aqui" and a counter with value 0', () => {
+    renderWithRedux(<App />);
+    const clickBtn = screen.getByRole('button', { name: 'Clique aqui' });
+    expect(screen.getByText('0')).toBeInTheDocument();
+    expect(clickBtn).toBeInTheDocument();
+  });
+  it('Should increment the value of clicks, after clicking it', () => {
+    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 } } });
+    const clickBtn = screen.getByRole('button', { name: 'Clique aqui' });
+    expect(screen.getByText('5')).toBeInTheDocument();
+    userEvent.click(clickBtn);
+    expect(screen.getByText('6')).toBeInTheDocument();
+  })
+})
