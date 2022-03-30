f66cf0bfb6e2c99bcf74a65807d9e992d49a562a
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-30 16:10:46 -0300

    finalizada revisao

--- 02-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-actions-assincronas/15.4-exercise/reddit-bonus/src/store/actions/index.js
+++ 02-front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-actions-assincronas/15.4-exercise/reddit-bonus/src/store/actions/index.js
@@ -1,19 +1,29 @@
 export const REQUEST_API = 'REQUEST_API';
 export const GET_API_RESPONSE = 'GET_API_RESPONSE';
+export const GET_TITLES = 'GET_TITLES';
+export const GET_FAILURE = 'GET_FAILURE';
+export const SET_THEME = 'SET_THEME';
 
 export const requestAPI = () => ({ type: REQUEST_API });
 
-export const getTitles = (data) => ({ type: GET_API_RESPONSE, data });
+export const getApiResponse = (subResponse, current) => ({ type: GET_API_RESPONSE, subResponse });
+
+export const getTitles = () => ({ type: GET_TITLES });
+
+export const getFailure = (msg) => ({ type: GET_FAILURE, msg })
+
+export const setTheme = (theme) => ({ type: SET_THEME, theme });
 
 export const fetchAPI = (subreddit) => {
   return async (dispatch) => {
     dispatch(requestAPI());
+    dispatch(setTheme(subreddit));
     try {
       const response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
       const data = await response.json();
-      return dispatch(getTitles(data));
+      return dispatch(getApiResponse(data));
     } catch(error) {
-      console.log(`Falha encontrada: ${error}`);
+      return dispatch(getFailure(error));
     }
   }
 }
