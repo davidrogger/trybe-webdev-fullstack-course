d2f06a71f7bef24eba74813d3dcf101c21017108
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-06 10:54:35 -0300

    feat(block22.2):io-local
    add readFile with Promise all content

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-fluxo-assincrono/22.2-exercises/io-local/readFile.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-fluxo-assincrono/22.2-exercises/io-local/readFile.js
@@ -12,14 +12,30 @@
 // });
 
 // second model
+// const fs = require('fs').promises;
+
+// const fileName = 'my-file.txt';
+
+// fs.readFile(fileName, 'utf8')
+//   .then((data) => console.log(`File content: ${data}`))
+//   .catch((err) => {
+//     console.error(`It was not possible to read the file ${fileName}\n Error: ${err}`);
+//     process.exit(1); // ends the script execution and report the system about the error.
+//   });
+
+// third model
+
 const fs = require('fs').promises;
 
-const fileName = 'my-file.txt';
-
-fs.readFile(fileName, 'utf8')
-  .then((data) => console.log(`File content: ${data}`))
+Promise.all([
+  fs.readFile('file1.txt'),
+  fs.readFile('file2.txt'),
+  fs.readFile('file3.txt'),
+])
+  .then(([file1, file2, file3]) => {
+    const fileSizeSum = file1.byteLength + file2.byteLength + file2.byteLength;
+    console.log(`Read three files total ${fileSizeSum} bytes`);
+  })
   .catch((err) => {
-    console.error(`It was not possible to read the file ${fileName}\n Error: ${err}`);
-    process.exit(1); // ends the script execution and report the system about the error.
+    console.error(`Error reading file: ${err.message}`);
   });
-
