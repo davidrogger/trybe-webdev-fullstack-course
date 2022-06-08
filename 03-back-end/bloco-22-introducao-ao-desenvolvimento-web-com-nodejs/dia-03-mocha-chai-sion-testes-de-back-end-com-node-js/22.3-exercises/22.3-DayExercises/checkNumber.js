16c4ec5873e6f26c9700619e7c3dbf3930c79b9e
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-08 13:05:35 -0300

    feat(block22.3):exercises
    add second exercicse

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-03-mocha-chai-sion-testes-de-back-end-com-node-js/22.3-exercises/22.3-DayExercises/checkNumber.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-03-mocha-chai-sion-testes-de-back-end-com-node-js/22.3-exercises/22.3-DayExercises/checkNumber.js
@@ -1,5 +1,17 @@
 function checkNumber(number) {
-  //
+
+  try {
+    if(!number && number !== 0) throw new Error('The function should have a number parameter!')
+    const baseNumber = 0;
+    if(number > baseNumber) return 'positive';
+    if(number < baseNumber) return 'negative';
+    return 'neutral';
+  } catch (err) {
+    console.log(`Something went wrong. \n Error: ${err.message}`)
+  }
+
 }
+
+checkNumber(0);
 
 module.exports = checkNumber;
