9570c4edc55be674b0637f8cd1365a140a1e700a
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-09 16:18:40 -0300

    feat(block22.4):exercise
    add second day exercise

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/express-exercise/src/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/express-exercise/src/index.js
@@ -3,10 +3,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const users = { "name": ''};
+
 app.use(bodyParser.json())
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Hello, ${name}` })
+
 });
 
 app.listen(3000, () => {
