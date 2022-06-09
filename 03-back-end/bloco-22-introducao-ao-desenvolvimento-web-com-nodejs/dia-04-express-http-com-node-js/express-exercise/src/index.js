42fbf74b28f50af42b889c76266c80984a48a12a
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-09 18:37:56 -0300

    feat(block22.4):exercise
    add fourth exercise day

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/express-exercise/src/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/express-exercise/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const app = express();
 
 const users = { "name": ''};
 
-app.use(bodyParser.json())
+app.use(express.json())
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
@@ -22,6 +22,11 @@ app.post('/greetings', (req, res) => {
   if (Number(age) > 17) return res.status(200).json({ message: `Hello, ${name}!` });
   res.status(401).json({ message: 'Unauthorized' });
 })
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
 
 app.listen(3000, () => {
   console.log('Listening Port 3000');
