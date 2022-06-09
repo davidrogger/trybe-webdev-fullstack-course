13ef7240c99368196098b038b38e3e83eaee6205
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-09 17:13:25 -0300

    feat(block22.4):exercise
    add third exercise day

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/express-exercise/src/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/express-exercise/src/index.js
@@ -17,6 +17,12 @@ app.post('/hello', (req, res) => {
 
 });
 
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (Number(age) > 17) return res.status(200).json({ message: `Hello, ${name}!` });
+  res.status(401).json({ message: 'Unauthorized' });
+})
+
 app.listen(3000, () => {
   console.log('Listening Port 3000');
 });
