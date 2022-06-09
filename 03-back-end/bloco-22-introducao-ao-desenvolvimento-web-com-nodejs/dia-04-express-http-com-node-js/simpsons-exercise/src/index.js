1278b78a44cda0fbcf418b55e33283ae5ef60f2d
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-09 19:24:30 -0300

    feat(block22.4):exercise-simpsons
    add post new caracter

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/simpsons-exercise/src/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/simpsons-exercise/src/index.js
@@ -3,11 +3,34 @@ const fs = require('fs');
 
 const app = express();
 
-const data = JSON.parse(fs.readFileSync('./simpsons.json'));
+const fileName = './simpsons.json';
+
+const data = JSON.parse(fs.readFileSync(fileName));
+
+app.use(express.json());
 
 app.get('/simpsons', (req, res) => {
   if(!data) return res.status(500).send();
   res.status(200).json(data);
+});
+
+app.post('/simpsons', (req, res) => {
+
+  const reqValidation = Object.keys(req.body);
+  const maxKeys = 2;
+  if(reqValidation.length > maxKeys) return res.status(500).json({ message: 'Exceeded the value of keys needed' });
+
+  const { id, name } = req.body;
+
+  const indexId = data.findIndex(( item ) => item.id === id);
+  const idNotFound = -1;
+  if(indexId !== idNotFound) return res.status(409).json({ message: `id ${id} already exists` });
+
+  const newData = [...data, { id, name }];
+  fs.writeFileSync(fileName, JSON.stringify(newData));
+  // res.status(200).json({ message: `${name} add with sucess!` });
+  res.status(204).end();
+
 });
 
 app.get('/simpsons/:id', (req, res) => {
