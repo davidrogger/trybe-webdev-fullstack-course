ef8020c07fe63a59351fcf3313238f652fd4b88a
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-09 19:01:46 -0300

    feat(block22.4):exercise-simpsons
    add endpoint simpsons id

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/simpsons-exercise/src/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-node-js/simpsons-exercise/src/index.js
@@ -3,10 +3,23 @@ const fs = require('fs');
 
 const app = express();
 
+const data = JSON.parse(fs.readFileSync('./simpsons.json'));
+
 app.get('/simpsons', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('./simpsons.json'));
   if(!data) return res.status(500).send();
   res.status(200).json(data);
+});
+
+app.get('/simpsons/:id', (req, res) => {
+  const { id } = req.params;
+  const indexId = data.findIndex(( item ) => item.id === id);
+  
+  const idNotFound = -1;
+  if(indexId === idNotFound) return res.status(404).json({ message: `Id ${id} Not Found` });
+
+  const idFound = data[indexId];
+
+  res.status(200).json(idFound);
 });
 
 app.listen(4000, () => {
