e176475832dd5df31bd727ac04445c3ff7ace852
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-10 08:36:57 -0300

    feat(block22.5):index
    add validation price

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/recipe-app/src/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/recipe-app/src/index.js
@@ -3,11 +3,28 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const invalidData = { message: 'Invalid data!' };
+
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
   { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
+
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  if(!name) return res.status(400).json(invalidData);
+  next();
+};
+
+const validatePrice = (req, res, next) => {
+  const { price } = req.body;
+  const priceNumber = Number(price);
+  const isNegative = priceNumber < 0;
+  const isNotANumber = typeof priceNumber !== 'number';
+  if(isNegative && isNotANumber) return res.status(400).json(invalidData);
+  next()
+}
 
 app.get('/recipes', function (req, res) {
   res.status(200).json(recipes);
@@ -27,13 +44,13 @@ app.get('/recipes/:id', function (req, res) {
   res.status(200).json(recipe);
 });
 
-app.post('/recipes', function (req, res) {
+app.post('/recipes', validateName, validatePrice, function (req, res) {
   const { id, name, price, waitTime } = req.body;
   recipes.push({ id, name, price, waitTime});
   res.status(201).json({ message: 'Recipe created successfully!'});
 });
 
-app.put('/recipes/:id', function (req, res) {
+app.put('/recipes/:id', validateName,validatePrice , function (req, res) {
   const { id } = req.params;
   const { name, price, waitTime } = req.body;
   const recipeIndex = recipes.findIndex((r) => r.id === Number(id));
