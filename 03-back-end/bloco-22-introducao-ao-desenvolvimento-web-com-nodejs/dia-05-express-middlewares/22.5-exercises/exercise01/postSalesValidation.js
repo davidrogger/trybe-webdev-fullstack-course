1d9d58a285fa726aa48cd28906f309837d4be78a
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-12 10:38:18 -0300

    feat(block22.5):exercise01
    exercicise info is required

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/22.5-exercises/exercise01/postSalesValidation.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/22.5-exercises/exercise01/postSalesValidation.js
@@ -1,3 +1,25 @@
+const moment = require('moment');
+
+const propertyRequired = (item, res) => {
+  const itemName = Object.keys(item).join();
+  if ( !item[itemName] ) {
+    res.status(400).json({ message: `${itemName} is required.` });
+    return true;
+  }
+  return false;
+}
+
+const salesDateCheck = (date, res) => {
+  const [day, month, year ] = date.split('/');
+  
+  if( !day || !month || !year) return true;
+  
+  if( !moment(date).isValid()) return true;
+
+  return false;
+
+}
+
   const postSalesValidation = (req, res, next) => {
 
     if(Object.keys(req.body).length === 0) {
@@ -5,17 +27,28 @@
       return;
     }
 
-    const { productName }  = req.body;
+    const { productName, infos }  = req.body;
 
-    if ( !productName ) {
-      res.status(400).json({ message: "productName is required  " });
-      return;
-    }
+    if(propertyRequired({ productName }, res)) return;
+
     if ( productName.length < 4 ) {
       res.status(400).json({ message: "producName need to have four caracter at least." 
     });
     return;
-  }
+  };
+
+  if(propertyRequired({ infos }, res)) return;
+
+  const { saleDate, warrantyPeriod } = infos;
+
+  if(propertyRequired({ saleDate }, res)) return;
+
+  if(salesDateCheck( saleDate, res )) {
+    res.status(400).json({ message: "Please saleDate should be valid and have a default pattern 'dd/mm/yyyy'" });
+    return;
+  };
+
+  if(propertyRequired({ warrantyPeriod }, res)) return;
 
     next();
 };
