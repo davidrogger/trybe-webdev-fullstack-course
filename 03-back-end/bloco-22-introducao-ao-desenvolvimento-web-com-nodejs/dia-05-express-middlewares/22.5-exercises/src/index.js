f167b96f4532217b8920a36327c181f0fb7fdbb2
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-13 14:06:25 -0300

    feat:signup
    new route to signup

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/22.5-exercises/src/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/22.5-exercises/src/index.js
@@ -4,7 +4,9 @@ const app = express();
 app.use(express.json());
 
 const salesRouter = require('../exercise01/salesRouter');
+const signupRouter = require('../exercise02/signup');
 
 app.use('/sales', salesRouter);
+app.use('/signup', signupRouter);
 
 app.listen(4000, () => { console.log('Listening at port 4000'); });
