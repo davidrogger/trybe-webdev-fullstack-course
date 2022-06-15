eee9c9a59736568331a75bc7022895174d037b62
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-15 16:51:02 -0300

    feat:userRouter
    add some importations and routers

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-01-introducao-arquitetura-de-software-camada-de-model/23.1-Exercises/23.1-model_exercise/src/userRouter.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-01-introducao-arquitetura-de-software-camada-de-model/23.1-Exercises/23.1-model_exercise/src/userRouter.js
@@ -2,13 +2,37 @@ const express = require('express');
 const router = express.Router();
 
 const valueValidation = require('../middlewares/valueValidation');
+const passwordValidation = require('../middlewares/passwordValidation');
+
+const User = require('../models/User');
+
+router.get('/', async (req, res) => {
+  const users = await User.getAll();
+  res.status(200).json(users);
+})
 
 router.post('/', [
   valueValidation,
-  (req, res) => {
+  passwordValidation,
+  async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
+  
+  
+  await User.create(firstName, lastName, email, password);
 
+  const { id } = await User.lastId();
+  const newUser = { id, firstName, lastName, email, password };
+  res.status(201).json(newUser);
   
 }]);
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+
+  if (!user) return res.status(404).json({ error: true, message: 'User not found' });
+
+  res.status(200).json(user);
+});
 
 module.exports = router;
