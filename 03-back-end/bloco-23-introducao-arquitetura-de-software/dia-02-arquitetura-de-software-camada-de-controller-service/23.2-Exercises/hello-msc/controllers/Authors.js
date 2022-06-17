8cf42552e65e850e6712368073cd44c426f800a4
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-17 09:55:45 -0300

    feat:Authors
    add to the Authors controllers logic that fullfilled the service error approach

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/controllers/Authors.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/controllers/Authors.js
@@ -6,14 +6,14 @@ const getAll = async (_req, res) => {
   res.status(200).json(authors)
 };
 
-const findById = async (req, res) => {
+const findById = async (req, res, next) => {
   const { id } = req.params;
 
   const author = await Author.findById(id);
 
-  if (!author) return res.status(404).json({ message: 'Author not found' });
+  if (author.error) return next(author.error);
 
-  res.status(200).json(author);
+  return res.status(200).json(author);
 };
 
 const createAuthor = async (req, res) => {
