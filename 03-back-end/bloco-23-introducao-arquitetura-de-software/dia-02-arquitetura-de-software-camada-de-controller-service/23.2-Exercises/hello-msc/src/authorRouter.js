029710193e39144180fd8ea342eb29337f1e71ef
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-17 15:36:34 -0300

    fix:authorRouter
    remove unneeded importation

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/src/authorRouter.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/src/authorRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
 const rescue = require('express-rescue');
 const errorMiddleware = require('../middlewares/error');
 
 const Author = require('../controllers/Authors');
-const { append } = require('express/lib/response');
 
 router.get('/', rescue(Author.getAll));
 router.get('/:id', rescue(Author.findById));
