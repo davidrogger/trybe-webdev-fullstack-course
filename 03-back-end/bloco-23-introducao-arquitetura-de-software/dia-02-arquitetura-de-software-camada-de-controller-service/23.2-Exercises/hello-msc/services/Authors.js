573b01dee4140be0df186b8bab87ae55a50e6cf1
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-17 09:49:00 -0300

    feat:Authors
    add to the Authors service error when the full name author already exists

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/services/Authors.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/services/Authors.js
@@ -5,7 +5,20 @@ const getAll = async () => Author.getAll();
 
 const findById = async (id) => Author.findById(id);
 
-const create = async (firstName, middleName, lastName) =>
-  Author.create(firstName, middleName, lastName);
+const create = async (firstName, middleName, lastName) => {
+  const existingAuthor = await Author.create(firstName, middleName, lastName);
+
+  if (existingAuthor) {
+    return {
+      error: {
+        code: 'alreadyExists',
+        message: 'Author with this name already exists',
+      },
+    };
+  }
+
+  return Author.create(firstName, middleName, lastName);
+
+}
 
 module.exports = { create, findById, getAll };
