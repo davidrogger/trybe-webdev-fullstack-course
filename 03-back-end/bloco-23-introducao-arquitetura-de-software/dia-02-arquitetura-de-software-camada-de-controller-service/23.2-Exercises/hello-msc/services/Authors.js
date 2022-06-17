cc9052605f64713ea5116e84c6bc8611364fad51
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-17 09:52:53 -0300

    feat:Authors
    add to the Authors service error when the id is noit found

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/services/Authors.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/services/Authors.js
@@ -3,7 +3,21 @@ const Author = require('../models/Author');
 
 const getAll = async () => Author.getAll();
 
-const findById = async (id) => Author.findById(id);
+const findById = async (id) => {
+  const author = await Author.findById(id);
+
+  if (!author) {
+    return {
+      error: {
+        code: 'notFound',
+        message: `It\s not possible to find the author id: ${id}.`,
+      },
+    };
+  }
+
+  return author;
+
+}
 
 const create = async (firstName, middleName, lastName) => {
   const existingAuthor = await Author.create(firstName, middleName, lastName);
