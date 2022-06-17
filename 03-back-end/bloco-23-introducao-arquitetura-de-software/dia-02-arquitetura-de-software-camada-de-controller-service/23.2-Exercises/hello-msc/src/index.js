986adbfae3e253dcf00c6957f6ebf65e1795afff
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-17 15:37:09 -0300

    feat:index
    add books router to the index

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/src/index.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/hello-msc/src/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 
 const authorsRouter = require('./authorRouter');
+const booksRouter = require('./bookRouter');
 
 const PORT = 3000;
 
 app.use(express.json());
 
 app.use('/authors', authorsRouter);
+app.use('/books', booksRouter)
 
 app.listen(PORT, () => {
   console.log(`Listening at PORT ${PORT}`);
