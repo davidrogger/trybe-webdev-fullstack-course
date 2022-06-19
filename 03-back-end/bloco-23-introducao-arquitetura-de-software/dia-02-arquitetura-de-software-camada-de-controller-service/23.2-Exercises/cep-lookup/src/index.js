1ab2b6075e2b07cdff5e1f892b0119b8216a3839
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-19 10:59:27 -0300

    feat:index main
    add express json

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/src/index.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+
 const app = express();
 
 const PORT = process.env.port || 3000;
 
 const routers = require('../routers/index');
 
+app.use(express.json());
 app.use(routers);
 
 app.listen(PORT, () => {
