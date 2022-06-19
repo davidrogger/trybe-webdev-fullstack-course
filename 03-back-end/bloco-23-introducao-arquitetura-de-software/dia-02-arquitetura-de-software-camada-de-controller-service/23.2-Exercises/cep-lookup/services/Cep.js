30f12c0a594fa18ae06de7f3f082f9422d276843
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-19 14:37:04 -0300

    feat: cep service
    create function to call the model and return the value if find some data, if not, return null

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/services/Cep.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/services/Cep.js
@@ -1,4 +1,13 @@
-const find = ({ cep }) => cep;
+const Cep = require('../models/Cep');
+
+const find = async (cep) => {
+  const cepData = await Cep.find(cep);
+  const notFound = 0;
+
+  if (cepData.length === notFound) return null;
+
+  return cepData[0];
+};
 
 module.exports = {
   find,
