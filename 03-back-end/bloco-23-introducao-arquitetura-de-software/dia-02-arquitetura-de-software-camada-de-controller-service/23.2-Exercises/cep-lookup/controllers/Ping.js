6f5a2febec83c545e32d611da50eaa182482c2fe
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-19 10:58:18 -0300

    fix:ping
    clean indentation

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/controllers/Ping.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/controllers/Ping.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const { HTTP_OK_RESPONSE } = require('../status/status');
 
 router.get('/', (_req, res) => {
-  res.status(HTTP_OK_RESPONSE).json({ message: 'pong' });
-})
+  res
+    .status(HTTP_OK_RESPONSE)
+    .json({ message: 'pong' });
+});
 
 module.exports = router;
