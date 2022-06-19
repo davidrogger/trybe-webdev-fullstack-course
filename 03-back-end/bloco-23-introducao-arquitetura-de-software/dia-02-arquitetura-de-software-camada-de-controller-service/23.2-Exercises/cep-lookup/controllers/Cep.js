cc0cf0285c727d2441cb42b7e9b3cec51ad3897a
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-19 14:40:14 -0300

    feat: cep controller
    add request to service and condictional about the response receive

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/controllers/Cep.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/controllers/Cep.js
@@ -5,12 +5,19 @@ const cepValidation = require('../middleware/cepValidation');
 const router = express.Router();
 
 const CepService = require('../services/Cep');
-const { HTTP_OK_RESPONSE } = require('../status/status');
+const { HTTP_OK_RESPONSE, HTTP_NOT_FOUND } = require('../status/status');
 
 router.get('/:cep', [
   cepValidation,
   async (req, res) => {
-    const data = await CepService.find(req.params);
+    const data = await CepService.find(req.cep);
+    
+    if (!data) {
+ return res
+    .status(HTTP_NOT_FOUND)
+    .json({ error: { code: 'notFound', message: 'CEP não encontrado' } }); 
+}
+    
     res.status(HTTP_OK_RESPONSE).json(data);
 }]);
 
