4470c733142ef3e556f59b2e67d8b3a22a0c7c3b
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-19 11:09:21 -0300

    feat:index routers
    add new route cep

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/routers/index.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/routers/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 
 const pingController = require('../controllers/Ping');
+const cepController = require('../controllers/Cep');
 
 router.use('/ping', pingController);
+router.use('/cep', cepController);
 
 module.exports = router;
