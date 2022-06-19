97b9b8950ec9c5fd50f03d2ed8e69468c0f5e87f
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-19 14:37:43 -0300

    fix:cepValidation
    fix logic to return the correct answer after the tests

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/middleware/cepValidation.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/middleware/cepValidation.js
@@ -1,4 +1,4 @@
-const { HTTP_BAD_RESQUEST } = require('../status/status');
+const { HTTP_BAD_REQUEST } = require('../status/status');
 
 const cepValidation = (req, res, next) => {
   const { cep } = req.params;
@@ -9,11 +9,13 @@ const cepValidation = (req, res, next) => {
   const cepClean = cep.replace('-', '');
   const lengthTest = cepClean.length === length;
 
-  if (cepFormatTest || lengthTest) {
+  if (!cepFormatTest || !lengthTest) {
     return res
-      .status(HTTP_BAD_RESQUEST)
+      .status(HTTP_BAD_REQUEST)
       .json({ error: { code: 'invalidData', message: 'CEP inválido' } });
   }
+
+  req.cep = cepClean;
   next();
 };
 
