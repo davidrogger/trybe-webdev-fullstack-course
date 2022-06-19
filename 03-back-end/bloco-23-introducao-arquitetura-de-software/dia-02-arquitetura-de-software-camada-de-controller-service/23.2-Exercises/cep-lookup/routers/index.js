efec64c7e9bbe04ff7728c65cbbf87b5ae869feb
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-19 17:12:51 -0300

    feat:index routers
    add rescue into the routers

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/routers/index.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/routers/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const rescue = require('express-rescue');
+const errorMiddleware = require('../middleware/error');
+
 const pingController = require('../controllers/Ping');
 const cepController = require('../controllers/Cep');
 
-router.use('/ping', pingController);
-router.use('/cep', cepController);
+router.use('/ping', rescue(pingController));
+router.use('/cep', rescue(cepController));
+
+router.use(errorMiddleware);
 
 module.exports = router;
