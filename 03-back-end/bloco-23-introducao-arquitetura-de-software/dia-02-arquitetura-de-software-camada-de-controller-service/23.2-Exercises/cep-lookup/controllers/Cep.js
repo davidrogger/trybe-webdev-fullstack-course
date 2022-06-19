e57692d0af6809ee0927ccb0f02307d3ec6efdf3
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-19 15:17:23 -0300

    feat: cep controllers
    create controller that validate information with joi and call cep services

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/controllers/Cep.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/controllers/Cep.js
@@ -1,11 +1,37 @@
 const express = require('express');
 
+const Joi = require('joi');
+
 const cepValidation = require('../middleware/cepValidation');
 
 const router = express.Router();
 
 const CepService = require('../services/Cep');
-const { HTTP_OK_RESPONSE, HTTP_NOT_FOUND } = require('../status/status');
+const { HTTP_OK_RESPONSE, HTTP_NOT_FOUND, HTTP_CREATED } = require('../status/status');
+
+router.post('/', async (req, res, next) => {
+  const { cep, logradouro, bairro, localidade, uf } = req.body;
+
+  const { error } = Joi.object({
+    cep: Joi.string().pattern(new RegExp('d{5}-d{3}')).not().empty(),
+    logradouro: Joi.string().not().empty().required(),
+    bairro: Joi.string().not().empty().required(),
+    localidade: Joi.string().not().empty().required(),
+    uf: Joi.string().not().empty().required(),
+  }).validate({ cep, logradouro, bairro, localidade, uf });
+
+  if (error) {
+    return next(error);
+  }
+
+  const newData = { cep, logradouro, bairro, localidade, uf };
+
+  const savedCep = await CepService.create(newData);
+
+  if (savedCep.error) return next(savedCep.error);
+
+  return res.status(HTTP_CREATED).json(savedCep);
+});
 
 router.get('/:cep', [
   cepValidation,
