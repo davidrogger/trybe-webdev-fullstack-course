38970acb8e72d4da54b19921986045691ee12009
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-20 09:58:45 -0300

    feat: findexternalCep
    add function to get cep from external api

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/services/Cep.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-02-arquitetura-de-software-camada-de-controller-service/23.2-Exercises/cep-lookup/services/Cep.js
@@ -1,12 +1,50 @@
-const { cleanCep } = require('../helpers/cleanCep');
+const axios = require('axios');
 const Cep = require('../models/Cep');
+const { cleanCep } = require('../helpers/cleanCep');
+
+const findExternalCep = async (cep) => {
+  try {
+    const URL = ` https://viacep.com.br/ws/${cep}/json/`;
+    const foundCep = await axios.get(URL);
+    return foundCep.data;    
+  } catch (error) {
+    return error;
+  }
+};
+
+const checkApiResponse = async (response) => {
+  switch (true) {
+    case typeof response.erro !== 'undefined': {
+      return null;
+    }
+    case typeof response.cep !== 'undefined': {
+      return Cep.create(response);
+    }
+    default: {
+      return response;
+    }
+  }
+};
 
 const find = async (cep) => {
   const cepStandardizad = cleanCep(cep);
   const cepData = await Cep.find(cepStandardizad);
   const notFound = 0;
 
-  if (cepData.length === notFound) return null;
+  if (cepData.length === notFound) {
+    const apiCep = await findExternalCep(cep);
+    if (apiCep.message) {
+      return {
+        error: {
+          code: 'BadRequest',
+          message: apiCep.message,
+          url: apiCep.config.url,
+        },
+    }; 
+}
+    const standardFormat = { ...apiCep, cep: cleanCep(apiCep.cep) };
+    return checkApiResponse(standardFormat);
+  }
 
   return cepData[0];
 };
