94a3e336d7b0194e0165c330d18f660428f561e0
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-20 18:58:53 -0300

    feat:Products
    add route list-products calling getAll from model

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/controllers/Products.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/controllers/Products.js
@@ -2,4 +2,11 @@ const { Router } = require('express');
 
 const router = Router();
 
+const ProductModel = require('../models/productModel');
+
+router.get('/list-products', async (req, res) => {
+  const products = await ProductModel.getAll();
+  res.send(products);
+});
+
 module.exports = router;
