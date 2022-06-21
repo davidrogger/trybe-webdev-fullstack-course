d0330d2c06867c85ed992824bd0f2f6e690bf976
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-21 10:51:33 -0300

    fix:connection
    fix database name to connect

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/models/connection.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/models/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   user: 'root',
   password: '@mysql2022',
-  database: 'rest_exercises'
+  database: 'rest_exercicios'
 });
 
 module.exports = connection;
