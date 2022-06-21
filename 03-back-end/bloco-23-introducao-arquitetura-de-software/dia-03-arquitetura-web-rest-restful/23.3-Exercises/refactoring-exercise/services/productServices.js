51b8ffe1196bf9f5756e885535a058baa759cba9
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-21 13:40:11 -0300

    feat:productServices
    create function getbyid into the service layer

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/services/productServices.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/services/productServices.js
@@ -2,6 +2,21 @@ const productModel = require('../models/productModel');
 
 const getAll = () => productModel.getAll();
 
+const getById = async (id) => {
+  try {
+    const response = await productModel.getById(id);
+    if (response.error) return response;
+    const [product] = response;
+    if (product.length === 0) {
+      return { status: 404, product: { message: `Product ID ${id} NotFound` }};
+    }
+    return { status: 200, product: product[0] };
+  } catch (error) {
+    return { error };
+  }
+};
+
 module.exports = {
   getAll,
+  getById,
 };
