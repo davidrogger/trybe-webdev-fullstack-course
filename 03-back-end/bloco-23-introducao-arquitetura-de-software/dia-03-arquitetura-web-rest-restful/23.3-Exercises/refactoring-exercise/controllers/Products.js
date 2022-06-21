3ac6107dac591622b0163dd9fe3844503fff6362
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-21 10:37:36 -0300

    fix:Products
    change variable name to something that make more sense

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/controllers/Products.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/controllers/Products.js
@@ -2,13 +2,13 @@ const { Router } = require('express');
 
 const router = Router();
 
-const ProductModel = require('../services/productServices');
+const ProductServices = require('../services/productServices');
 
 // Removido 'list-products' do endpoint pois uma requisição do tipo GET,
 // sem parâmetros que especifiquem um recurso (ex: id de um produto),
 // já indica o retorno de uma coleção de recursos.
 router.get('/', async (_req, res) => {
-  const products = await ProductModel.getAll();
+  const products = await ProductServices.getAll();
   res.send(products);
 });
 
@@ -16,24 +16,24 @@ router.get('/', async (_req, res) => {
 // com parâmetros que especifiquem um recurso (ex: id de um produto),
 // já indica o retorno de um recurso.
 router.get('/:id', async (req, res) => {
-  const product = await ProductModel.getById(req.params.id);
+  const product = await ProductServices.getById(req.params.id);
   res.send(product);
 });
 
 router.post('/add-product', async (req, res) => {
   const { name, brand } = req.body;
-  const newProduct = await ProductModel.add(name, brand);
+  const newProduct = await ProductServices.add(name, brand);
   res.send(newProduct);
 });
 
 router.post('/delete-product/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
+  const products = await ProductServices.exclude(req.params.id);
   res.send(products);
 });
 
 router.post('/update-product/:id', async (req, res) => {
   const { name, brand } = req.body;
-  const products = await ProductModel.update(req.params.id, name, brand);
+  const products = await ProductServices.update(req.params.id, name, brand);
   res.send(products);
 });
 
