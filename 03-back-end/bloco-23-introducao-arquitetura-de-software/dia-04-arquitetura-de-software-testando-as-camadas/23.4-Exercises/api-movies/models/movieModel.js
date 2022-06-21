6ef7228a27ffd2ec23aeba1236557ddbe761f168
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-21 19:48:57 -0300

    wip:movieModel
    removing try catch to understand the test behaviour

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-04-arquitetura-de-software-testando-as-camadas/23.4-Exercises/api-movies/models/movieModel.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-04-arquitetura-de-software-testando-as-camadas/23.4-Exercises/api-movies/models/movieModel.js
@@ -1,7 +1,7 @@
 const connection = require('./connection');
 
 const create = async ({ title, directedBy, releaseYear }) => {
-  try {
+  // try {
     const query = `
     INSERT INTO movies (title, directed_by, release_year)
     VALUES
@@ -9,9 +9,9 @@ const create = async ({ title, directedBy, releaseYear }) => {
     `;
     const [movie] = await connection.execute(query, [title, directedBy, releaseYear]);  
     return { id: movie.insertId };    
-  } catch (error) {
-    return { error };
-  }
+  // } catch (error) {
+  //   return { error };
+  // }
 };
 
 module.exports = {
