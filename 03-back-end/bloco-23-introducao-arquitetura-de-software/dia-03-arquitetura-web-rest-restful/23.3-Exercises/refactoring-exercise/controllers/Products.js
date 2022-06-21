3ab25a11b3aef933bf424bcc32123c5e9511572f
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-21 13:40:44 -0300

    wip:Products
    change the way is handle the response and errors

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/controllers/Products.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-03-arquitetura-web-rest-restful/23.3-Exercises/refactoring-exercise/controllers/Products.js
@@ -3,23 +3,29 @@ const { Router } = require('express');
 const router = Router();
 
 const ProductServices = require('../services/productServices');
+const errorMiddleware = require('../middlewares/errorMiddleware');
 
 // Removido 'list-products' do endpoint pois uma requisição do tipo GET,
 // sem parâmetros que especifiquem um recurso (ex: id de um produto),
 // já indica o retorno de uma coleção de recursos.
-router.get('/', async (_req, res) => {
-  const products = await ProductServices.getAll();
+router.get('/', async (_req, res, next) => {
+  const response = await ProductServices.getAll();
+
+  if (response.error) return next(response.error);
   // Utilizamos o status code 200 para indicar que a requisição foi concluída
   // com sucesso e padronizamos o retorno para json.
-  res.status(200).json(products);
+  return res.status(200).json(response);
 });
 
 // Removido 'get-by-id' do endpoint pois uma requisição do tipo GET,
 // com parâmetros que especifiquem um recurso (ex: id de um produto),
 // já indica o retorno de um recurso.
-router.get('/:id', async (req, res) => {
-  const product = await ProductServices.getById(req.params.id);
-  res.send(product);
+router.get('/:id', async (req, res, next) => {
+  const response = await ProductServices.getById(req.params.id);
+
+  if (response.error) return next(response.error)
+  
+  res.status(response.status).json(response.product);
 });
 
 router.post('/add-product', async (req, res) => {
@@ -38,5 +44,7 @@ router.post('/update-product/:id', async (req, res) => {
   const products = await ProductServices.update(req.params.id, name, brand);
   res.send(products);
 });
+
+router.use(errorMiddleware);
 
 module.exports = router;
