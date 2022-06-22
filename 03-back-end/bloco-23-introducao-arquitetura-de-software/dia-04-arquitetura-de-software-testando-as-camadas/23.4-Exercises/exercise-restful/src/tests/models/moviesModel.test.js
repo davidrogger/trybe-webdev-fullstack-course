152d911a138e36d89567a7bc8cb75bc13bc2b3f8
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-22 18:50:15 -0300

    feat: moviesModel test
    movie model test to get a film information by id

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-04-arquitetura-de-software-testando-as-camadas/23.4-Exercises/exercise-restful/src/tests/models/moviesModel.test.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-04-arquitetura-de-software-testando-as-camadas/23.4-Exercises/exercise-restful/src/tests/models/moviesModel.test.js
@@ -1,41 +1,65 @@
 const { expect } = require("chai");
+const sinon = require('sinon');
 
-// empty dummy
-const MoviesModel = {
-  getById: () => {}
-};
+// application functions
+const connection = require('../../models/connection');
+const MoviesModel = require('../../models/moviesModel');
 
-const INVALID_ID = 100;
-const VALID_ID = 1;
+const TEST_ID = 1;
 
 describe('Get a film information from an "id"', () => {
   describe('When is not found any information with that id', () => {    
+
+    before( async () => {
+      const execute = [[]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+
+    after( async () => {
+      connection.execute.restore();
+    });
+
     it('Should return an "object"', async () => {
-      const response = await MoviesModel.getById(INVALID_ID);
+      const response = await MoviesModel.getById(TEST_ID);
       expect(response).to.be.a('object');
     });
 
     it('Should be an empty "object"', async () => {
-      const response = await MoviesModel.getById(INVALID_ID);
+      const response = await MoviesModel.getById(TEST_ID);
       expect(Object.keys(response)).to.have.length(0);
     });
   });
 
   describe('When the information is get it with sucess', () => {
-    const payloadMovie = {
+    const dbPayloadMovie = {
+      title: 'Test Title',
+      created_by: 'Test Person',
+      release_year: 2022,
+    };
+
+    const standardPayloadMovie = {
       title: 'Test Title',
       createdBy: 'Test Person',
       releaseYear: 2022,
     };
 
+    before( async () => {
+      const execute = [[dbPayloadMovie]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+
+    after( async () => {
+      connection.execute.restore();
+    });
+
     
     it('Should return an "object"', async () => {
-      const response = await MoviesModel.getById(VALID_ID);
+      const response = await MoviesModel.getById(TEST_ID);
       expect(response).to.be.a('object');
     });
     it('Should have the title, createdBy and releaseYear information', async () => {
-      const response = await MoviesModel.getById(VALID_ID);
-      expect(response).to.deep.equal(payloadMovie);
+      const response = await MoviesModel.getById(TEST_ID);
+      expect(response).to.deep.equal(standardPayloadMovie);
     })
   });
 
