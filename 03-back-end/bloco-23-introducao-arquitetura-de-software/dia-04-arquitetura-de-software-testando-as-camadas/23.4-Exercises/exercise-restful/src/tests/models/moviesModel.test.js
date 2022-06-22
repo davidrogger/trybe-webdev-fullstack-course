5fc6ab26ddd1aa834cc867d3ace3c0d7d62e8770
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-22 14:24:30 -0300

    feat: moviesModel test
    create the file to test get id model functionality

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-04-arquitetura-de-software-testando-as-camadas/23.4-Exercises/exercise-restful/src/tests/models/moviesModel.test.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-04-arquitetura-de-software-testando-as-camadas/23.4-Exercises/exercise-restful/src/tests/models/moviesModel.test.js
@@ -1,1 +1,42 @@
+const { expect } = require("chai");
 
+// empty dummy
+const MoviesModel = {
+  getById: () => {}
+};
+
+const INVALID_ID = 100;
+const VALID_ID = 1;
+
+describe('Get a film information from an "id"', () => {
+  describe('When is not found any information with that id', () => {    
+    it('Should return an "object"', async () => {
+      const response = await MoviesModel.getById(INVALID_ID);
+      expect(response).to.be.a('object');
+    });
+
+    it('Should be an empty "object"', async () => {
+      const response = await MoviesModel.getById(INVALID_ID);
+      expect(Object.keys(response)).to.have.length(0);
+    });
+  });
+
+  describe('When the information is get it with sucess', () => {
+    const payloadMovie = {
+      title: 'Test Title',
+      createdBy: 'Test Person',
+      releaseYear: 2022,
+    };
+
+    
+    it('Should return an "object"', async () => {
+      const response = await MoviesModel.getById(VALID_ID);
+      expect(response).to.be.a('object');
+    });
+    it('Should have the title, createdBy and releaseYear information', async () => {
+      const response = await MoviesModel.getById(VALID_ID);
+      expect(response).to.deep.equal(payloadMovie);
+    })
+  });
+
+});
