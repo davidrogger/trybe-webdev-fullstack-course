c9868550b4f5f77e2912b1fe41769dc0bfa4563f
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-22 09:28:40 -0300

    feat:moveController tests
    create test when the payload is valid and have sucess

--- 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-04-arquitetura-de-software-testando-as-camadas/23.4-Exercises/api-movies/tests/controllers/movieController.test.js
+++ 03-back-end/bloco-23-introducao-arquitetura-de-software/dia-04-arquitetura-de-software-testando-as-camadas/23.4-Exercises/api-movies/tests/controllers/movieController.test.js
@@ -30,6 +30,30 @@ describe('Ao camar o controller de create', () => {
   });
 
   describe('quando o payload informado é valido', () => {
+    const response = {};
+    const request = {};
 
+    before(() => {
+      request.body = {
+        title: 'Example Movie',
+        directedBy: 'Jane Dow',
+        releaseYear: 1999,
+      };
+
+      response.status = sinon.stub().returns(response);
+      response.send = sinon.stub().returns();
+    });
+
+    it('é chamado o status com o código 201', async () => {
+      await MoviesController.create(request, response);
+
+      expect(response.status.calledWith(201)).to.be.equal(true);
+    });
+
+    it('é chamado o sendo com mensagme "Filme criado com sucesso"', async () => {
+      await MoviesController.create(request, response);
+
+      expect(response.send.calledWith('Filme criado com sucesso!')).to.be.equal(true);
+    });
   });
 });
