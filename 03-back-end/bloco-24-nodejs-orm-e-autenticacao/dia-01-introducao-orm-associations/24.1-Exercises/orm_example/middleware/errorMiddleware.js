237cc13822a5066d2e773f7faace0670fdfbe6d1
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-24 10:20:50 -0300

    feat: file responsable to handle errors
    create file that handle resquest send to the error treatment

---
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_example/middleware/errorMiddleware.js
@@ -0,0 +1,11 @@
+module.exports = (err, _req, res, _next) => {
+  const statusGlossary = {
+    NOT_FOUND: 404,
+    BAD_RESQUEST: 500,
+  };
+  
+
+  const status = statusGlossary[err.status] || statusGlossary[BAD_RESQUEST];
+
+  res.status(status).json({ message: err.message });
+};
