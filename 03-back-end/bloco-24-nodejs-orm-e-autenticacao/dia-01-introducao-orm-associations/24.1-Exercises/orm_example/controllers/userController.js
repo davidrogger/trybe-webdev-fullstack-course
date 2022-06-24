0913dfc53fc2407f0a498facf97b8f00f3de952d
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-24 10:28:28 -0300

    feat: create function findByIdAndEmail
    create function that find a specific ID and email indo the data base

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_example/controllers/userController.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_example/controllers/userController.js
@@ -6,6 +6,7 @@ const messageGlossary = {
 
 const status = {
   HTTP_OK: 200,
+  HTTP_CREATED: 201,
   HTTP_NOT_FOUND: 404,
 }
 
@@ -14,6 +15,19 @@ const userController = {
     try {
       const { id } = req.params;
       const user = await User.findByPk(id);
+
+      if (!user) return next(messageGlossary.notFound);
+
+      return res.status(status.HTTP_OK).json(user);
+    } catch (error) {
+      next({ status: 'BAD_REQUEST', message: error.message  });
+    }
+  },
+  async findByIdAndEmail (req, res, next) {
+    try {
+      const { id } = req.params;
+      const { email } = req.query;
+      const user = await User.findONe( { where: { id, email } } );
 
       if (!user) return next(messageGlossary.notFound);
 
