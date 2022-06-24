f7e30d65631153dbaf2dc04be5e167d9eecd1f82
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-24 10:51:44 -0300

    feat: main file
    created file responsable to start the application

---
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_example/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = Number(process.env.PORT || 3000);
+
+const userRouter = require('../routers/userRouters');
+const errorMiddleware = require('../middleware/errorMiddleware');
+
+app.use(express.json());
+
+app.use('/user', userRouter);
+
+app.use(errorMiddleware);
+
+app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
+
