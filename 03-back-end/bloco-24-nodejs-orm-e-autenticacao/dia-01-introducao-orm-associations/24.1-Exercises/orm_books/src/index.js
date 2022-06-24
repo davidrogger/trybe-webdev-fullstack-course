e7855b10f2deb4456a9582b9d2fcbeb011bff6ce
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-24 19:54:02 -0300

    feat: add bookRouter in the application
    set bookRouter in the application index

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_books/src/index.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_books/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bookRouter = require('./routers/bookRouter');
 
 const app = express();
 
 const PORT = Number(process.env.PORT || 3000)
 
 app.use(express.json());
+
+app.use('/books', bookRouter);
 
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
