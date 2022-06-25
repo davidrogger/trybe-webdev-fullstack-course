030e40dd8c2b58d3ca39edd79d597a77eea2e163
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-25 10:49:57 -0300

    feat: add new route to request
    new route responsable to create a new book in the database

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_books/src/routers/bookRouter.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_books/src/routers/bookRouter.js
@@ -6,5 +6,6 @@ const bookRouter = Router();
 
 bookRouter.get('/', bookController.getAll); // Lista todos livros
 bookRouter.get('/:id', bookController.getById); // Mostra um livro com base em seu id
+bookRouter.post('/', bookController.create); // Cadastra no banco de dados um novo livre com base no corpo do post
 
 module.exports = bookRouter;
