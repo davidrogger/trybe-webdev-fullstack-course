26694ce73e7bf035359ed654263ac90e4a35492a
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-26 10:18:20 -0300

    feat: new route to update a book
    route responsable to call the controller to update a book

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_books/src/routers/bookRouter.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_books/src/routers/bookRouter.js
@@ -7,5 +7,6 @@ const bookRouter = Router();
 bookRouter.get('/', bookController.getAll); // Lista todos livros
 bookRouter.get('/:id', bookController.getById); // Mostra um livro com base em seu id
 bookRouter.post('/', bookController.create); // Cadastra no banco de dados um novo livre com base no corpo do post
+bookRouter.put('/:id', bookController.update) // Atualiza informações com base no corpo, de um livro pelo ID infornecido.
 
 module.exports = bookRouter;
