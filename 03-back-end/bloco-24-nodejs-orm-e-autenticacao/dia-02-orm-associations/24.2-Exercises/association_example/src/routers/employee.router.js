a9b3fc801d2b328d3a239b33b00c13f5bf2c46d1
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-27 19:25:14 -0300

    feat: start new route with employee
    configure new route to get info by id

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/24.2-Exercises/association_example/src/routers/employee.router.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/24.2-Exercises/association_example/src/routers/employee.router.js
@@ -4,5 +4,6 @@ const employeeController = require('../controllers/employee.controller');
 const employeeRouter = Router();
 
 employeeRouter.get('/', employeeController.getAll);
+employeeRouter.get('/:id', employeeController.getById);
 
 module.exports = employeeRouter;
