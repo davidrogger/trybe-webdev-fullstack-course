2270ecb8074822f6f5461dcc5409bf2e4cb8cd58
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-27 13:58:15 -0300

    feat: sequelize initial configuration
    move file to a src diretory and configure the sequelizerc to direct where the files are

---
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/24.2-Exercises/association_example/src/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports =
+{
+  "development": {
+    "username": process.env.MYSQL_USERNAME,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQLHOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.MYSQL_USERNAME,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQLHOST,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.MYSQL_USERNAME,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQLHOST,
+    "dialect": "mysql"
+  }
+}
