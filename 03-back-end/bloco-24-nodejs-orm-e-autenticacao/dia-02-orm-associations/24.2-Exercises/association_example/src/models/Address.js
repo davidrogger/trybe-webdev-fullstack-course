65f1ecec711b60a72523371c7f3b358af1332a97
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-27 15:08:32 -0300

    feat: created model to address
    function sequelize responsable to get info from the database.

---
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/24.2-Exercises/association_example/src/models/Address.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize/types");
+
+module.exports = (sequelize, DataType) => {
+  const Address = sequelize.define('Address', {
+    id: { type: DataType.INTEGER, primaryKey: true, autoIncrement: true },
+    city: DataType.STRING,
+    street: DataType.STRING,
+    number: DataTypes.STRING,
+    employeeId: { type: DataType.INTEGER, foreignKey: true },
+  },
+  {
+    timestamps: false,
+    tableName: 'Addresses',
+    undersconre: true,
+  });
+
+  Address.associate = (models) => {
+    Address.belongsTo(models.Employee,
+      {foreignKey: 'employeeId', as: 'employees'},
+    );
+  }
+
+  return Address;
+};
