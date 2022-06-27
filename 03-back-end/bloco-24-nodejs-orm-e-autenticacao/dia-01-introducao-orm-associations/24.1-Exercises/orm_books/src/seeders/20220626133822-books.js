788dedf2aca4a97cf526e0be4cca79007efb9307
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-27 07:26:26 -0300

    feat: add more itens in the seeder
    add more books to test others routers

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_books/src/seeders/20220626133822-books.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-introducao-orm-associations/24.1-Exercises/orm_books/src/seeders/20220626133822-books.js
@@ -10,6 +10,20 @@ module.exports = {
       created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
       updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
     },
+    {
+      title: 'Done',
+      author: 'Josep',
+      page_quantity: 63,
+      created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    {
+      title: 'Breakfast',
+      author: 'Ego',
+      page_quantity: 50,
+      created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
   ], {}),
 
   down: async (queryInterface) => queryInterface.bulkDelete('Books', null, {}),
