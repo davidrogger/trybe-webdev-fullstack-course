7c7dabb17872fc940f54979cd64619bd07b45cd7
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-28 14:58:57 -0300

    feat: configure tests at the sequelize config
    add database test and logging in the sequelize configurations

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/24.2-Exercises/association_example/src/config/config.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/24.2-Exercises/association_example/src/config/config.js
@@ -12,9 +12,11 @@ module.exports =
   "test": {
     "username": process.env.MYSQL_USERNAME,
     "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
+    "database": process.env.MYSQL_TEST_DATABASE,
     "host": process.env.MYSQLHOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    // logging omite mensagens de log no orm
+    "logging": false
   },
   "production": {
     "username": process.env.MYSQL_USERNAME,
