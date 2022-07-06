a2a68e78979bc5de750ba3b68eca2f41b37c1ae8
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-06 17:28:32 -0300

    fix: usar name revised
    apply the correcter key username

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/24.3-Exercises/hello-jwt/src/database/config/config.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/24.3-Exercises/hello-jwt/src/database/config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 const dataConfig = {
-  usarname: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.HOSTNAME,
-  dialect: process.env.DBLANGUAGE,
+  "username": process.env.MYSQL_USERNAME,
+  "password": process.env.MYSQL_PASSWORD,
+  "database": process.env.MYSQL_DATABASE,
+  "host": process.env.HOSTNAME,
+  "dialect": process.env.DBLANGUAGE,
 };
 
 module.exports = {
