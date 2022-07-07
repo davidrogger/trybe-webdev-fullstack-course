464761cca67ead4e154de62fa08a487c85a01bee
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-07 09:59:16 -0300

    feat: route login
    route responsable to authorize the login sending a token

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/24.3-Exercises/hello-jwt/src/controllers/user.controller.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/24.3-Exercises/hello-jwt/src/controllers/user.controller.js
@@ -1,11 +1,18 @@
 const validate= require("../services/validate.service");
 const userService = require('../services/user.service');
 
+
 module.exports = {
   async create(req, res) {
     const user = await validate.userData(req.body);
     const newUser = await userService.create(user);
     
-    res.status(201).json({ message: `Welcome ${newUser}!` });
+    res.status(201).json({ message: `Welcome ${newUser.username}!` });
+  },
+  async login(req, res) {
+    const user = await validate.userData(req.body);
+    const userLogin = await userService.login(user);
+    const token = userService.tokenGeneretor(userLogin);
+    res.status(200).json(token);
   },
 };
