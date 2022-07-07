d282cd3931e7cd113a1fededbc5502352a2c90be
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-07 09:26:43 -0300

    feat: standardize snake case to the database
    apply the standard snake case into the database

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/24.3-Exercises/hello-jwt/src/database/models/user.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/24.3-Exercises/hello-jwt/src/database/models/user.js
@@ -1,7 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-   name: DataTypes.STRING,
-   password: DataTypes.STRING, 
+    name: DataTypes.STRING,
+    password: DataTypes.STRING, 
+    admin: DataTypes.BOOLEAN,
+  },
+  {
+    underscored: true,
+    tableName: 'Users'
   });
 
   return User;
