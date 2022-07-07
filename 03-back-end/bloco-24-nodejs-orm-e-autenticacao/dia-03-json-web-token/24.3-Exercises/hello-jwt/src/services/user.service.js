151d24971d34ce2a6a834e8bb60a5fe74b912b19
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-07 09:58:21 -0300

    fix: wrong key to create a new user

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/24.3-Exercises/hello-jwt/src/services/user.service.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/24.3-Exercises/hello-jwt/src/services/user.service.js
@@ -1,5 +1,6 @@
 const { User } = require('../database/models');
 const jwt = require('jsonwebtoken');
+const { ErrorNotFound } = require('../errors/errorsLibrary');
 
 const SECRET = process.env.SECRET;
 
@@ -13,12 +14,21 @@ module.exports = {
     return jwt.sign({ data: user }, SECRET, jwtConfig);
   },
   async create ({ username, password }) {
-    const newUser = await User.create({ username, password });
+    const newUser = await User.create({ name: username, password });
 
     if (newUser) {
       return { username };
     }
 
     throw new Error('Something went wrong, please try again later');
+  },
+  async login ({ username, password }) {
+    const user = await User.findOne({ where: { name: username, password } });
+    
+    if (user) {
+      return { username: user.name, admin: user.admin };
+    }
+
+    throw new ErrorNotFound('User not found or incorrect password');
   },
 };
