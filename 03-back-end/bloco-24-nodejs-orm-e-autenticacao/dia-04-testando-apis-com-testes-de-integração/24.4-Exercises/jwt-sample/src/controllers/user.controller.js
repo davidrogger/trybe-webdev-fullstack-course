1979fcffc1725690e4426ee4b6afe10087d80e93
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-10 16:12:54 -0300

    feat: change the function to generate the token
    now the token generation is made by a centralized function that handle all jwt

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integração/24.4-Exercises/jwt-sample/src/controllers/user.controller.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integração/24.4-Exercises/jwt-sample/src/controllers/user.controller.js
@@ -1,11 +1,16 @@
 const validateService = require('../services/validate.service');
 const userService = require('../services/user.service');
+const { tokenGenerate } = require('../helpers/tokenJWT');
 
 module.exports = {
   async login(req, res) {
     const userBodyChecked = await validateService.loginBodyFormat(req.body);
     const user = await userService.findUser(userBodyChecked);
-    const token = userService.tokenGenerate(user);
+    const token = tokenGenerate(user);
     res.status(200).json({ message: `Welcome ${user.name}`, token });
+  },
+  async getAll(req, res) {
+    const users = await userService.getAll();
+    res.status(200).json(users);
   },
 };
