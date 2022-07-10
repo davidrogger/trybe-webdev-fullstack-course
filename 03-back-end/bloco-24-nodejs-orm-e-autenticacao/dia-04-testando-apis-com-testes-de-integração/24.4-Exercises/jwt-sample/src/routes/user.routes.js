910ee49f780c1db6b565b7fc410f2eca9d13f91b
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-10 16:15:11 -0300

    feat:  add new routes
    route responsabel to verify authentication and a net route to get all users.

--- 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integração/24.4-Exercises/jwt-sample/src/routes/user.routes.js
+++ 03-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integração/24.4-Exercises/jwt-sample/src/routes/user.routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 require('express-async-errors');
 const userController = require('../controllers/user.controller');
+const authentication = require('../middlewares/authentication');
 
 const userRoute = Router();
 
 userRoute.post('/login', userController.login);
+userRoute.use(authentication);
+userRoute.get('/', userController.getAll);
 
 module.exports = userRoute;
