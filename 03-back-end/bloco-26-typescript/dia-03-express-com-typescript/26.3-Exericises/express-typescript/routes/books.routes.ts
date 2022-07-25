c094628539ba96cec34fc09f3f4fffa38f24bf9f
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-25 10:14:07 -0300

    feat: final route to books getall

--- 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/26.3-Exericises/express-typescript/routes/books.routes.ts
+++ 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/26.3-Exericises/express-typescript/routes/books.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import BookController from '../controllers/books.controller';
 
 const route = Router();
+
+const booksController = new BookController();
+
+route.get('/books', booksController.getAll);
 
 export default route;
