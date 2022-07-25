58ea6c16570d96fafa9c4062e4d659e252d6cead
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-25 09:36:22 -0300

    improve: errorsHandler
    break in parts

--- 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/26.3-Exericises/express-typescript/index.ts
+++ 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/26.3-Exericises/express-typescript/index.ts
@@ -1,5 +1,6 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import errosHandler from './middlewares/errorsHandler';
 import 'express-async-errors';
 
 const app = express();
@@ -12,27 +13,7 @@ app.get('/', (req: Request, res: Response) => {
   res.status(StatusCodes.OK).send('Express + TypeScript')
 });
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  const { name, message, details } = err as any;
-  console.log(`name: ${name}`);
-
-  switch(name) {
-    case 'ValidationError':
-      res.status(StatusCodes.BAD_REQUEST).json({ message: details[0].message });
-      break;
-    case 'NotFoundError':
-      res.status(StatusCodes.NOT_FOUND).json({ message });
-      break;
-    case 'ConflictError':
-      res.status(StatusCodes.CONFLICT).json({ message });
-      break;
-    default:
-      console.error(err);
-      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
-  }
-
-  next();
-});
+app.use(errosHandler);
 
 app.listen(PORT, () => console.log(`Online: ${PORT}`));
 
---
+++ 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/26.3-Exericises/express-typescript/middlewares/errorsHandler.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+export default (err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  console.log(`name: ${name}`);
+
+  switch(name) {
+    case 'ValidationError':
+      res.status(StatusCodes.BAD_REQUEST).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(StatusCodes.NOT_FOUND).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(StatusCodes.CONFLICT).json({ message });
+      break;
+    default:
+      console.error(err);
+      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+  }
+
+  next();
+}
