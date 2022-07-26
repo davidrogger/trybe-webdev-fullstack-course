15d0f3cc5b193cb0c868421c49f3de5f236ffdf8
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-26 18:56:44 -0300

    feat: change library to handle errors to the route

--- 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/26.3-Exericises/type-practice-one/src/app.ts
+++ 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/26.3-Exericises/type-practice-one/src/app.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
+import { Router, json } from 'express';
 import UserRoute from './routes/user.route';
-import 'express-async-errors';
 import errorHandler from './middlewares/errorHandler';
 
 const app = Router();
 
+app.use(json());
 app.use(UserRoute)
 app.use(errorHandler)
 
