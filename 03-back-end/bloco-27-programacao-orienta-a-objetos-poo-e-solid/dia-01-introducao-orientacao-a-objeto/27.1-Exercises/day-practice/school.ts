fa0c8658022193d70c66eb744fc7b60eb8e4dd30
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-28 15:29:53 -0300

    feat exercise 05

--- 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-01-introducao-orientacao-a-objeto/27.1-Exercises/day-practice/school.ts
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-01-introducao-orientacao-a-objeto/27.1-Exercises/day-practice/school.ts
@@ -44,22 +44,23 @@ class Pedido {
   cliente: string;
   itens: itemPedido[];
   pagamento: string;
-  desconto?: number;
+  desconto: number;
 
-  constructor(c: string, i: itemPedido[], p: string) {
+  constructor(c: string, i: itemPedido[], p: string, desconto: number = 0) {
     this.cliente = c;
     this.itens = i;
     this.pagamento = p;
+    this.desconto = desconto;
   }
 
   total(): number {
     return this.itens.reduce((acc, item) => acc + item.preco,0);
   }
 
-  totalComDesconto(): number {
+  totalComDesconto(desconto?: number): string {
     const total = this.total();
-    const desconto = this.desconto ? this.desconto : 0;
-    return total * desconto;
+    if (desconto) this.desconto = desconto;
+    return `Total com desconto de ${this.desconto} %: ${total - (total * (this.desconto / 100))}`;
   }
 
 }
@@ -73,3 +74,11 @@ class itemPedido {
   }
 }
 
+const batata = new itemPedido('batata', 10);
+const acai = new itemPedido('acai', 15);
+
+const pedido1 = new Pedido('Jonas', [batata, acai], 'avista');
+
+console.log(pedido1.total());
+console.log(pedido1.totalComDesconto());
+console.log(pedido1.totalComDesconto(10));
