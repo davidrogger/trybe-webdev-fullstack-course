071c98be5a5aca4fe806c7c12df06050d583a4ed
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-28 10:49:12 -0300

    improve: create new method
    create a method to reduce the repeated lines

--- 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/26.3-Exericises/type-practice-one/src/services/validation.service.ts
+++ 03-back-end/bloco-26-typescript/dia-03-express-com-typescript/26.3-Exericises/type-practice-one/src/services/validation.service.ts
@@ -1,28 +1,24 @@
 import { StatusCodes } from "http-status-codes";
-import Joi from "joi";
-import IUser, { InewUser } from "../interface/user.interface";
+import Joi, { Schema } from "joi";
 import ErrorCustom from "../utils/error";
 
-const validate = {
-  idFormat(id: number): void {
-    const { error } = Joi.number().positive().integer().required().label('id')
-    .validate(id);
-    if (error) {
-      throw new ErrorCustom(StatusCodes.BAD_REQUEST, error.message);
-    }
-  },
-  userFormat(user: InewUser): InewUser {
-    const { error, value } = Joi.object({
-      name: Joi.string().min(3).required(),
-      email: Joi.string().email().required(),
-      password: Joi.string().min(6).max(12).required(),
-    }).validate(user);
-    if (error) {
-      throw new ErrorCustom(StatusCodes.BAD_REQUEST, error.message);
-    }
+const runSchema = (schema: Schema) => (itemScan: {}) => {
+  const { error, value } = schema.validate(itemScan);
 
-    return value;
-  },
+  if (error) throw new ErrorCustom(StatusCodes.BAD_REQUEST, error.message);
+
+  return value;
+}
+
+const validate = {
+  idFormat: runSchema(Joi.object({
+    id: Joi.number().positive().integer().required(),
+  })),
+  userFormat: runSchema(Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(12).required(),
+  })),
 }
 
 export default validate;
