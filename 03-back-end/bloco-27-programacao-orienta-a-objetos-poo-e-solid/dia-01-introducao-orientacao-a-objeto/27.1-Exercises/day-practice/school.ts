b5c669c1d3ed80e8a0cf9459cad4531a024a48cb
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-28 15:14:12 -0300

    feat: excercise 4

--- 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-01-introducao-orientacao-a-objeto/27.1-Exercises/day-practice/school.ts
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-01-introducao-orientacao-a-objeto/27.1-Exercises/day-practice/school.ts
@@ -1,7 +1,7 @@
 type notas = [number, number, number, number]
 type trabalhos = [number, number]
 
-class Disciplina {
+class Estudante {
   matricula: number;
   nome: string;
   notas;
@@ -27,8 +27,49 @@ class Disciplina {
 
 }
 
-const aluno1 = new Disciplina(123, 'Jonas', [5, 6, 7, 5], [8, 6]);
+const aluno1 = new Estudante(123, 'Jonas', [5, 6, 7, 5], [8, 6]);
 
-console.log(aluno1);
-console.log(aluno1.somaNotas());
-console.log(aluno1.mediaNotas());
+// console.log(aluno1);
+// console.log(aluno1.somaNotas());
+// console.log(aluno1.mediaNotas());
+
+class Cliente {
+ nome: string;
+ constructor(n: string) {
+  this.nome = n;
+ }
+}
+
+class Pedido {
+  cliente: string;
+  itens: itemPedido[];
+  pagamento: string;
+  desconto?: number;
+
+  constructor(c: string, i: itemPedido[], p: string) {
+    this.cliente = c;
+    this.itens = i;
+    this.pagamento = p;
+  }
+
+  total(): number {
+    return this.itens.reduce((acc, item) => acc + item.preco,0);
+  }
+
+  totalComDesconto(): number {
+    const total = this.total();
+    const desconto = this.desconto ? this.desconto : 0;
+    return total * desconto;
+  }
+
+}
+
+class itemPedido {
+  nome: string;
+  preco: number;
+  constructor(n: string, p: number) {
+    this.nome = n;
+    this.preco = p;
+  }
+}
+
