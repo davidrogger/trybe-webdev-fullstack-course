e80546c5442a0fe22282ac2a532f7c3987ea9025
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-28 13:59:53 -0300

    feat: add sum and average methods

--- 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-01-introducao-orientacao-a-objeto/27.1-Exercises/day-practice/school.ts
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-01-introducao-orientacao-a-objeto/27.1-Exercises/day-practice/school.ts
@@ -14,6 +14,21 @@ class Disciplina {
     this.trabalhos = trabalhos;
   };
 
+  somaNotas(): number {
+    const sum = this.notas.reduce((acc, nota) => acc + nota, 0);
+    return sum;
+  }
+
+  mediaNotas(): number {
+    const qtNotas = this.notas.length;
+    const somaNotas = this.somaNotas();
+    return somaNotas/qtNotas;
+  }
+
 }
 
 const aluno1 = new Disciplina(123, 'Jonas', [5, 6, 7, 5], [8, 6]);
+
+console.log(aluno1);
+console.log(aluno1.somaNotas());
+console.log(aluno1.mediaNotas());
