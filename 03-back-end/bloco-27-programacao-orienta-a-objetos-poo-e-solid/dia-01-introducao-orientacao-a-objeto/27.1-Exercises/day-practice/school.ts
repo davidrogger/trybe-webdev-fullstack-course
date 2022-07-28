5dd16f42accd5b4d0a051a40620e973aa165f77c
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-28 18:52:06 -0300

    feat: organize files

--- 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-01-introducao-orientacao-a-objeto/27.1-Exercises/day-practice/school.ts
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-01-introducao-orientacao-a-objeto/27.1-Exercises/day-practice/school.ts
@@ -29,56 +29,6 @@ class Estudante {
 
 const aluno1 = new Estudante(123, 'Jonas', [5, 6, 7, 5], [8, 6]);
 
-// console.log(aluno1);
-// console.log(aluno1.somaNotas());
-// console.log(aluno1.mediaNotas());
-
-class Cliente {
- nome: string;
- constructor(n: string) {
-  this.nome = n;
- }
-}
-
-class Pedido {
-  cliente: string;
-  itens: itemPedido[];
-  pagamento: string;
-  desconto: number;
-
-  constructor(c: string, i: itemPedido[], p: string, desconto: number = 0) {
-    this.cliente = c;
-    this.itens = i;
-    this.pagamento = p;
-    this.desconto = desconto;
-  }
-
-  total(): number {
-    return this.itens.reduce((acc, item) => acc + item.preco,0);
-  }
-
-  totalComDesconto(desconto?: number): string {
-    const total = this.total();
-    if (desconto) this.desconto = desconto;
-    return `Total com desconto de ${this.desconto} %: ${total - (total * (this.desconto / 100))}`;
-  }
-
-}
-
-class itemPedido {
-  nome: string;
-  preco: number;
-  constructor(n: string, p: number) {
-    this.nome = n;
-    this.preco = p;
-  }
-}
-
-const batata = new itemPedido('batata', 10);
-const acai = new itemPedido('acai', 15);
-
-const pedido1 = new Pedido('Jonas', [batata, acai], 'avista');
-
-console.log(pedido1.total());
-console.log(pedido1.totalComDesconto());
-console.log(pedido1.totalComDesconto(10));
+console.log(aluno1);
+console.log(aluno1.somaNotas());
+console.log(aluno1.mediaNotas());
