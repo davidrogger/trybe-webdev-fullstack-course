a052c0daec759d79d4e5276b1b89fa9af22c360f
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-28 18:55:46 -0300

    feat: exercise 5

---
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-01-introducao-orientacao-a-objeto/27.1-Exercises/day-practice/date.ts
@@ -0,0 +1,11 @@
+class CustomDate {
+  day: number;
+  month: number;
+  year: number;
+
+  constructor(d: number, m: number, y: number) {
+    this.day = d;
+    this.month = m;
+    this.year = y;
+  }
+}
