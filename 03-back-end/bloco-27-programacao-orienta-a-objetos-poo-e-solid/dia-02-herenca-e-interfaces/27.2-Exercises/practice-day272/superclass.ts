384e0cc121a96622747bec5c47bd017987fa9595
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-31 10:00:40 -0300

    feat: practice part3

--- 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-02-herenca-e-interfaces/27.2-Exercises/practice-day272/superclass.ts
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-02-herenca-e-interfaces/27.2-Exercises/practice-day272/superclass.ts
@@ -7,17 +7,22 @@ class Superclass {
 }
 
 class Subclass extends Superclass{
+  constructor(){
+    super(false);
+  }
   public sayHello2() {
     this.sayHello();
   }
 }
 
-function myFunc(param: Subclass) {
-  param.sayHello2()
+function myFunc(param: Superclass) {
+  if (param.isSuper) console.log('Super!')
+  if (!param.isSuper) console.log('Sub!')
+  param.sayHello()
 }
 
-// const suupa = new Superclass();
+const suupa = new Superclass();
 const sub = new Subclass();
 
-// myFunc(suupa);
+myFunc(suupa);
 myFunc(sub);
