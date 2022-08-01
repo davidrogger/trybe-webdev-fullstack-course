edec463adfa35bb138ee46af45e7e06728137252
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-01 12:58:43 -0300

    feat: create new class Studant
    new class extends from Person

--- 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-02-herenca-e-interfaces/27.2-Exercises/272-day-practice/Person.ts
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-02-herenca-e-interfaces/27.2-Exercises/272-day-practice/Person.ts
@@ -26,3 +26,57 @@ class Person {
     this._name = value;
   }
 }
+
+class Student extends Person {
+  private _enrollment: string;
+
+  constructor(name: string, birthDate: Date) {
+      super(name, birthDate);
+      this.enrollment = this.enrollmentGenerator();
+    };
+
+    get enrollment():string {
+      return this._enrollment;
+    }
+
+    set enrollment(value: string) {
+      if (value.length < 16) throw new Error('Enrollment should have 16 caracteres');
+      this._enrollment = value;
+    }
+
+    enrollmentGenerator() {
+      return (
+        new Date().getTime().toString() +
+        new Date().getTime().toString())
+        .slice(0, 16);
+    }
+
+    get examsGrades(): number[] {
+      return this.examsGrades;
+    }
+
+    set examsGrades(value: number[]) {
+      if (value.length !== 4) throw new Error('Should have four grades');
+      this.examsGrades = value;
+    }
+
+    get worksGrades(): number[] {
+      return this.worksGrades;
+    }
+
+    set worksGrades(value: number[]) {
+      if (value. length !== 2) throw new Error('Should have two work grades');
+      this.worksGrades = value;
+    }
+
+    sumGrades(): number {
+      const totalGrades = [...this.examsGrades, ...this.worksGrades]
+        .reduce((acc, grade) => acc + grade,0);
+        return totalGrades;
+    }
+
+    get averageGrades(): number {
+      const gradesQuantity = this.examsGrades.length + this.worksGrades.length;
+      return this.sumGrades() / gradesQuantity;
+    }
+}
