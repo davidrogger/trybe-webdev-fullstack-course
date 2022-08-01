01c1007bea2296a147846499ef266d7cb5e78bfa
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-01 17:28:10 -0300

    feat: add new configurations
    with the test resulto to study

--- 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-02-herenca-e-interfaces/27.2-Exercises/272-day-practice/Student.ts
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-02-herenca-e-interfaces/27.2-Exercises/272-day-practice/Student.ts
@@ -1,7 +1,9 @@
 import Person from "./Person";
 
 export default class Student extends Person {
-  private _enrollment: string;
+  private _enrollment = String();
+  private _examsGrades: number[] = [];
+  private _worksGrades: number[] = [];
 
   constructor(name: string, birthDate: Date) {
       super(name, birthDate);
@@ -25,21 +27,21 @@ export default class Student extends Person {
     }
 
     get examsGrades(): number[] {
-      return this.examsGrades;
+      return this._examsGrades;
     }
 
     set examsGrades(value: number[]) {
       if (value.length !== 4) throw new Error('Should have four grades');
-      this.examsGrades = value;
+      this._examsGrades = value;
     }
 
     get worksGrades(): number[] {
-      return this.worksGrades;
+      return this._worksGrades;
     }
 
     set worksGrades(value: number[]) {
       if (value. length !== 2) throw new Error('Should have two work grades');
-      this.worksGrades = value;
+      this._worksGrades = value;
     }
 
     sumGrades(): number {
@@ -54,3 +56,65 @@ export default class Student extends Person {
     }
 }
 
+// const carolina = new Student('Carolina da Silva', new Date('2005/03/17'));
+
+
+// carolina.examsGrades = [10, 10, 10, 10]
+// console.log(carolina);
+
+// Gabarito
+
+// ./Student.ts
+
+// import Person from "./Person";
+
+// export default class Student extends Person {
+//   private _enrollment = String();
+//   private _examsGrades: number[] = [];
+//   private _worksGrades: number[] = [];
+
+//   constructor(name: string, birthDate: Date) {
+//     super(name, birthDate);
+//     this.enrollment = this.generateEnrollment();
+//   }
+
+//   get enrollment(): string {
+//     return this._enrollment;
+//   }
+
+//   set enrollment(value: string) {
+//     if (value.length < 16) throw new Error('A matrícula deve possuir no mínimo 16 caracteres.');
+
+//     this._enrollment = value;
+//   }
+
+//   get examsGrades(): number[] {
+//     return this._examsGrades;
+//   }
+
+//   set examsGrades(value: number[]) {
+//     if (value.length > 4) throw new Error('A pessoa estudante só pode possuir 4 notas de provas.');
+
+//     this._examsGrades = value;
+//   }
+
+//   get worksGrades(): number[] {
+//     return this._worksGrades;
+//   }
+
+//   set worksGrades(value: number[]) {
+//     if (value.length > 2) throw new Error('A pessoa estudante só pode possuir 2 notas de trabalhos.');
+
+//     this._worksGrades = value;
+//   }
+
+//   generateEnrollment(): string {
+//     const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+//     return `STU${randomStr}`;
+//   }
+// }
+
+// const carolina = new Student('Carolina da Silva', new Date('2005/03/17'));
+
+// console.log(carolina);
