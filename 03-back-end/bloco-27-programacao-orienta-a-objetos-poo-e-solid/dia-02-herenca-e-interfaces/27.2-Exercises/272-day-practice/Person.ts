f84cb0308eee4208e1fa41bd7cebf2ccbe899fb8
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-01 10:41:58 -0300

    feat: exercise creating a class person

---
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-02-herenca-e-interfaces/27.2-Exercises/272-day-practice/Person.ts
@@ -0,0 +1,28 @@
+class Person {
+  constructor(private _name: string, private _birthDate: Date) {
+    this.name = _name;
+    this.birthDate = _birthDate;
+  };
+
+  get birthDate(): Date {
+    return this._birthDate;
+  }
+
+  set birthDate(value: Date){
+    const today = new Date();
+    if (today < value) throw new Error('Date shouldn\'t be in the future');
+    const timeDiff = Math.abs(today.getTime() - value.getTime()); // diferença em milissegundos
+    const age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+    if (age > 120) throw new Error('Shouldn\'t have more than 120 years');
+    this._birthDate = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    if (value.length < 3) throw new Error('Name minimum lenght is 3');
+    this._name = value;
+  }
+}
