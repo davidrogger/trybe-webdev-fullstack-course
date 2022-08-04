fb181ebbea9b86104d0889f3fa550c0395dc689f
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-04 10:00:06 -0300

    feat: apply single responsability

--- 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-04-solid-intro-principios-s-o-d/274-Exercises/274-follow-material/src/ReadingTracker.ts
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-04-solid-intro-principios-s-o-d/274-Exercises/274-follow-material/src/ReadingTracker.ts
@@ -1,31 +1,15 @@
-type Book = {
-  book: string;
-  author: string;
-  genre: string;
-}
-
 export default class ReadingTracker {
   private readingGoal: number;
   private booksRead: number;
-  private wishlist: Book[];
 
   constructor(readingGoal: number) {
     this.readingGoal = readingGoal;
     this.booksRead = 0;
-    this.wishlist = [];
   }
 
   trackReadings(readsCount: number): void {
     this.booksRead += readsCount;
     console.log(this.progressNotification());
-  }
-
-  addToWishlist(book: Book): void {
-    this.wishlist.push(book);
-  }
-
-  showWishlist(): void {
-    console.log(this.wishlist);
   }
 
   progressNotification(): string {
@@ -39,7 +23,5 @@ export default class ReadingTracker {
 }
 
 const readTracker = new ReadingTracker(20);
-  readTracker.addToWishlist({ book: 'The Road', author: 'Cormac McCarthy', genre: 'Dystopia' });
-  readTracker.showWishlist();
   readTracker.trackReadings(12);
   readTracker.trackReadings(9);
---
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-04-solid-intro-principios-s-o-d/274-Exercises/274-follow-material/src/progressNotification.ts
@@ -0,0 +1,14 @@
+const typeLibrary: Record<string, string> = {
+  email: 'Email: ',
+  phone: 'Phone: ',
+  console: 'Console: ',
+};
+
+export default function progressNotification(
+  message: string, notificationType:string,
+  ): void {
+    const type = typeLibrary[notificationType.toLocaleLowerCase()] || '';
+    console.log(type + message);
+}
+
+progressNotification('batata', 'xa');
