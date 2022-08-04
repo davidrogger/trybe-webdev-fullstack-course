8d81f3cc2e3bdfdc7133e816857486a16e4ec5d7
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-04 08:21:17 -0300

    feat: appling single responsability

---
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-04-solid-intro-principios-s-o-d/274-Exercises/274-follow-material/src/ReadingTracker.ts
@@ -0,0 +1,45 @@
+type Book = {
+  book: string;
+  author: string;
+  genre: string;
+}
+
+export default class ReadingTracker {
+  private readingGoal: number;
+  private booksRead: number;
+  private wishlist: Book[];
+
+  constructor(readingGoal: number) {
+    this.readingGoal = readingGoal;
+    this.booksRead = 0;
+    this.wishlist = [];
+  }
+
+  trackReadings(readsCount: number): void {
+    this.booksRead += readsCount;
+    console.log(this.progressNotification());
+  }
+
+  addToWishlist(book: Book): void {
+    this.wishlist.push(book);
+  }
+
+  showWishlist(): void {
+    console.log(this.wishlist);
+  }
+
+  progressNotification(): string {
+    switch (true) {
+      case this.booksRead >= this.readingGoal:
+        return 'Congratulations! You\'ve reached your reading goal!';
+      default:
+        return 'There are still some books to go!';
+    }
+  }
+}
+
+const readTracker = new ReadingTracker(20);
+  readTracker.addToWishlist({ book: 'The Road', author: 'Cormac McCarthy', genre: 'Dystopia' });
+  readTracker.showWishlist();
+  readTracker.trackReadings(12);
+  readTracker.trackReadings(9);
