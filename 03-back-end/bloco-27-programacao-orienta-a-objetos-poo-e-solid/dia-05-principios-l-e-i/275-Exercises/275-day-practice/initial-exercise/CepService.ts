4d90e085ca1227dad6bab1daae0ebee4f36b92e7
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-06 13:58:03 -0300

    feat: practice initial structur to practice

---
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-05-principios-l-e-i/275-Exercises/275-day-practice/initial-exercise/CepService.ts
@@ -0,0 +1,16 @@
+import FooCepAPI from "./FooCepAPI";
+
+export default class CepService {
+  private readonly cepApi: FooCepAPI;
+  constructor() {
+    this.cepApi = new FooCepAPI();
+  }
+
+  addressByCep(cep: string, num: number) {
+    return this.cepApi.getAddressByCEP(cep, num);
+  }
+
+  cepByAddress(address: string, num: number) {
+    return this.cepApi.getCepByAddress(address, num);
+  }
+}
---
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-05-principios-l-e-i/275-Exercises/275-day-practice/initial-exercise/FooCepAPI.ts
@@ -0,0 +1,9 @@
+export default class FooCepAPI {
+  async getAddressByCEP(cep: string, number: number): Promise<string> {
+    return `O endereço para o "CEP: ${cep}, nº ${number}" é "endereço foo"`;
+  }
+
+  async getCepByAddress(address: string, number: number): Promise<string> {
+    return `O CEP para: "${address}, nº: ${number}" é "CEP baz"`;
+  }
+}
