7203744351ba35f1b6b60cbdd2e1a26f8a278b9f
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-08 09:56:59 -0300

    feat: create initial migration

---
+++ 03-back-end/bloco-27-programacao-orienta-a-objetos-poo-e-solid/dia-06-extra-practice-by-myself/276-sequelize-type/src/database/migration/20220808124927-create-Books.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      isbn: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Books');
+  }
+};
