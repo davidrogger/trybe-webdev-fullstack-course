e67c126353552f9804797a79263f164fba582271
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-04 14:59:47 -0300

    feat: list exercise

---
+++ 04-ciencia-da-computacao/session-01-introduction-python/day-01-learning-python/s01-d01-Exercises/list-operations.py
@@ -0,0 +1,9 @@
+trybe_course = ["Introdução", "Front-end", "Back-end"]
+
+# Exercício 5: Adicione o elemento “Ciência da Computação” à lista.
+
+trybe_course.append("Ciência da Computação")
+
+# Exercício 6: Acesse e altere o primeiro elemento da lista para “Fundamentos”.
+
+trybe_course[0] = "Fundamentos"
