d9fad6fe683415cbe4dde28573530df67eaa9715
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-10 10:17:54 -0300

    feat: exercise m04s01d01e01 done

---
+++ 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-01-learning-python/m04-s01-d01-Exercises/m04-s01-d01-exercise-day/m04s01d01e01.py
@@ -0,0 +1,9 @@
+# 🚀 Exercício 1:
+# Crie uma função que receba dois números e retorne o maior deles.
+
+
+def biggestNumber(number1, number2):
+    return max(number1, number2)
+
+
+print(biggestNumber(100, 50))
