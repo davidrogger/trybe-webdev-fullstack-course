f2406ce6324e7d34098bba3e07decb3e238fb214
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-11 14:40:24 -0300

    feat: printing insert name in vertical

---
+++ 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-02-data-output-input/m04s01d02Exercises/m04s01d01-follow-material/03-vertical_name.py
@@ -0,0 +1,9 @@
+# Exercício 1:
+# Faça um programa que solicite o nome de uma pessoa usuária
+# e imprima-o na vertical. Exemplo:
+
+
+name = input('Insert your name: ')
+
+for letter in name:
+    print(letter)
