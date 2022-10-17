dce751d0f737b4a1c2954db2bb50f80a4bf6fc0d
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-17 16:26:28 -0300

    test: list of invalid emails

--- 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-03-tests/m04s01d03Exercises/m04s01d03-exercise-day/test_m04s01d03e04.py
+++ 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-03-tests/m04s01d03Exercises/m04s01d03-exercise-day/test_m04s01d03e04.py
@@ -1,5 +1,20 @@
 from m04s01d03e04 import list_valid_emails
 
 
-def test_emails_invalids():
+def test_emails_empty_list():
+    "Should return an empty list when where is no e-mail in the list"
     assert list_valid_emails([]) == []
+
+
+def test_emails_invalids():
+    "Should return an empty list when all emails area invalid"
+    invalid_emails = [
+        "invalid#caracter@email.com",
+        "invalid/caracter@email.com",
+        "invalid.caracter@email.com",
+        "invalid$caracter@email.com",
+        "withoutatemail.com",
+        "extraextension@email.com.br",
+        "invalidextension@email.combr",
+    ]
+    assert list_valid_emails(invalid_emails) == []
