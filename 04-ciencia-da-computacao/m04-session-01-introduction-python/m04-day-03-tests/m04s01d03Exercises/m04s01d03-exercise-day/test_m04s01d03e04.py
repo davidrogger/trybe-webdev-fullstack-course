bf1ba8c2ff515d35cd7357b8fb159dfe8414856a
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-17 18:22:24 -0300

    test: valid emails list

--- 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-03-tests/m04s01d03Exercises/m04s01d03-exercise-day/test_m04s01d03e04.py
+++ 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-03-tests/m04s01d03Exercises/m04s01d03-exercise-day/test_m04s01d03e04.py
@@ -1,20 +1,33 @@
 from m04s01d03e04 import list_valid_emails
 
+invalid_emails = [
+    "invalid#caracter@email.com",
+    "invalid/caracter@email.com",
+    "invalid.caracter@email.com",
+    "invalid$caracter@email.com",
+    "withoutatemail.com",
+    "extraextension@email.com.br",
+    "invalidextension@email.combr",
+]
+
+valid_emails = [
+    "meu@email.com",
+    "joana@email.com",
+    "maria_fatima@email.com",
+    "davi-ribeiro@email.com",
+]
+
 
 def test_emails_empty_list():
-    "Should return an empty list when where is no e-mail in the list"
+    "Should return an empty list when there is no e-mail in the list"
     assert list_valid_emails([]) == []
 
 
 def test_emails_invalids():
-    "Should return an empty list when all emails area invalid"
-    invalid_emails = [
-        "invalid#caracter@email.com",
-        "invalid/caracter@email.com",
-        "invalid.caracter@email.com",
-        "invalid$caracter@email.com",
-        "withoutatemail.com",
-        "extraextension@email.com.br",
-        "invalidextension@email.combr",
-    ]
+    "Should return an empty list when all emails are invalid"
     assert list_valid_emails(invalid_emails) == []
+
+
+def test_emails_valids():
+    "Should return all emails in the list"
+    assert list_valid_emails(valid_emails) == valid_emails
