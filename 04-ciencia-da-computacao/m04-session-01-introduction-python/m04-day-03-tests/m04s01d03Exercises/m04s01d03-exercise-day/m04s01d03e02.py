8872c30a53f822615a74cb22bdc983f09e974ff9
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-17 10:43:49 -0300

    test: phone_convertion assertation tests done

--- 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-03-tests/m04s01d03Exercises/m04s01d03-exercise-day/m04s01d03e02.py
+++ 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-03-tests/m04s01d03Exercises/m04s01d03-exercise-day/m04s01d03e02.py
@@ -1,5 +1,28 @@
-def phone_convertion(phrase):
-    if len(phrase) == 0:
-        return ""
+numbers_catalog = {
+    "ABC": 2,
+    "DEF": 3,
+    "GHI": 4,
+    "JKL": 5,
+    "MNO": 6,
+    "PQRS": 7,
+    "TUV": 8,
+    "WXYZ": 9,
+}
 
-    return phrase
+
+def phone_convertion(phrase: str):
+    new_phrase = ""
+
+    if len(phrase) > 0:
+        for position, letter in enumerate(phrase):
+            for key, value in numbers_catalog.items():
+                key_found = key.rfind(letter)
+                if key_found >= 0:
+                    new_phrase += str(value)
+                    break
+            original_phrase_position = position
+            new_phrase_position = len(new_phrase) - 1
+            if new_phrase_position < original_phrase_position:
+                new_phrase += letter
+
+    return new_phrase
--- 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-03-tests/m04s01d03Exercises/m04s01d03-exercise-day/test_m04s01d03e02.py
+++ 04-ciencia-da-computacao/m04-session-01-introduction-python/m04-day-03-tests/m04s01d03Exercises/m04s01d03-exercise-day/test_m04s01d03e02.py
@@ -3,3 +3,22 @@ from m04s01d03e02 import phone_convertion
 
 def test_phone_convertion_empty_input():
     assert phone_convertion('') == ''
+
+
+def test_phone_convertion_cataloged_input():
+    number1 = phone_convertion('1-HOME-SWEET-HOME')
+    number2 = phone_convertion('MY-MISERABLE-JOB')
+    assert number1 == '1-4663-79338-4663'
+    assert number2 == '69-647372253-562'
+
+
+def test_phone_convertion_uncataloged_inputs():
+    number1 = phone_convertion('123456')
+    number2 = phone_convertion('-/*+,')
+    assert number1 == '123456'
+    assert number2 == '-/*+,'
+
+
+def test_phone_convertion_bigger_than_30_inputs():
+    number1 = phone_convertion('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG')
+    assert number1 == '843 78425 27696 369 58677 6837 843 5299 364'
