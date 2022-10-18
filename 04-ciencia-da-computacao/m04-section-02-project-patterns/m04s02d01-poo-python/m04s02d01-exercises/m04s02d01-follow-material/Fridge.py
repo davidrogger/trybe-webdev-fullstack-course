726dda059f4d5af627263410be810c2b6d2cdc5b
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-18 15:16:56 -0300

    test: creating a fridge pass case

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-follow-material/Fridge.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-follow-material/Fridge.py
@@ -1,2 +1,17 @@
 class Fridge:
-    ...
+    def __init__(self, color, voltage, price):
+        self.__color = color
+        self.__voltage = voltage
+        self.__price = price
+    
+    @property
+    def color(self):
+        return self.__color
+
+    @property
+    def voltage(self):
+        return self.__voltage
+    
+    @property
+    def price(self):
+        return self.__price
