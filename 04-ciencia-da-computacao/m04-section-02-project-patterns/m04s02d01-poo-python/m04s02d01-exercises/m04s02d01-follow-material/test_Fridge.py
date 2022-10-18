ac5582e32eefe27dd4f1b7a20d45b5bb276f4f99
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-18 15:10:15 -0300

    test: creating a fridge fail case

---
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-follow-material/test_Fridge.py
@@ -0,0 +1,7 @@
+from Fridge import Fridge
+
+def test_fridge_creation():
+    blue_fridge = Fridge('blue', '220', 2000)
+    assert blue_fridge.color == 'blue'
+    assert blue_fridge.voltage == '220'
+    assert blue_fridge.price == 2000
