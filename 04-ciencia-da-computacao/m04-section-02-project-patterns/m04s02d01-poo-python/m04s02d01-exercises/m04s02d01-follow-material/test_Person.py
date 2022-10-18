0a1ec477039d8a9f69b705652a9745a33cedbd17
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-18 15:35:21 -0300

    test: buying fridge without money fail case

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-follow-material/test_Person.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-follow-material/test_Person.py
@@ -1,7 +1,17 @@
 from Person import Person
+from Fridge import Fridge
+import pytest
+
 
 def test_person_creation():
-    jonas = Person('Jonas', 1000)
-    assert jonas.name == 'Jonas'
+    jonas = Person("Jonas", 1000)
+    assert jonas.name == "Jonas"
     assert jonas.wallet == 1000
-    assert jonas.has_fridge == False
+    assert jonas.has_fridge is False
+
+
+def test_person_buy_fridge_without_money():
+    poor_jonas = Person("Jonas", 1000)
+    blue_fridge = Fridge("Blue", "220", 2000)
+    with pytest.raises(ValueError, match="Insuficient resources"):
+        poor_jonas.buy_fridge(blue_fridge)
