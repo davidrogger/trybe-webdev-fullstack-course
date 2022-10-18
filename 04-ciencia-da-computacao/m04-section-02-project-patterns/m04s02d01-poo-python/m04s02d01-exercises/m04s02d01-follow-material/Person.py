97891f63a3e46001fedf8a43d6123a36eb5b79a1
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-18 15:27:30 -0300

    test: creating a Person pass case

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-follow-material/Person.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-follow-material/Person.py
@@ -1,2 +1,17 @@
 class Person:
-    ...
+    def __init__(self, name, wallet):
+        self.__name = name
+        self.__wallet = wallet
+        self.__has_fridge = False
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def wallet(self):
+        return self.__wallet
+
+    @property
+    def has_fridge(self):
+        return self.__has_fridge
