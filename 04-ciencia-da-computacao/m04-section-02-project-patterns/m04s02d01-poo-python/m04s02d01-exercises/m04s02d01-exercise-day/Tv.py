fdd6454e90a6819737b0968405c3ae727c9be46c
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-19 12:11:27 -0300

    test: creating a class Tv pass case

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/Tv.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/Tv.py
@@ -1,2 +1,10 @@
 class Tv:
-    ...
+    def __init__(self, size):
+        self.__size = size
+        self.__volume = 50
+        self.__channel = 1
+        self.__on = False
+
+    @property
+    def size(self):
+        return self.__size
