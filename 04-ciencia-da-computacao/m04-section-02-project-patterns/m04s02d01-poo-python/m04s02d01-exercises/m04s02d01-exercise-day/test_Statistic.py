f279e886ad102355b57127b0c57efcfeed3e0325
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-19 15:45:57 -0300

    test: method Statistic average fail case

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_Statistic.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_Statistic.py
@@ -19,3 +19,11 @@ def test_creating_statistic():
     "Should create an new instance when the parameter is correct"
     statistic_list = Statistic([10, 5, 6, 7, 8])
     assert isinstance(statistic_list, Statistic) is True
+
+
+def test_statistic_method_average():
+    "Should calculate the average grade of the studants"
+    jonas_grades = Statistic([10, 5, 6, 7, 8])
+    mary_grades = Statistic([10, 10, 10, 9, 9])
+    assert jonas_grades.average == 7.2
+    assert mary_grades.average == 9.6
