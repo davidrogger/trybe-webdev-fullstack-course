b5b0ff8be31c9bc76c8903afaa998d682bca1523
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-19 12:16:53 -0300

    chore: changed check size test describe

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_Tv.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_Tv.py
@@ -7,7 +7,7 @@ def test_tv_check_size_parameter():
     philips_42pol = Tv(42)
     samsung_54pol = Tv(54)
 
-    "Should have the size passed by the parameter"
+    "Should have the size passed by parameter"
     assert samsung_23pol.size == 23
     assert philips_42pol.size == 42
     assert samsung_54pol.size == 54
