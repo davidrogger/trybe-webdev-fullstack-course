802562950153f848a0cf3ea05c16b12b99001b95
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-19 16:06:33 -0300

    test: method Statistic median fail case

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_Statistic.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_Statistic.py
@@ -27,3 +27,17 @@ def test_statistic_method_average():
     mary_grades = Statistic([10, 10, 10, 9, 9])
     assert jonas_grades.average == 7.2
     assert mary_grades.average == 9.6
+
+
+def test_statistic_method_median():
+    "Should return the right median from an odd grades list"
+    jonas_grades = Statistic([10, 5, 6, 7, 8])
+    mary_grades = Statistic([10, 10, 10, 9, 9])
+    assert jonas_grades.median() == 7
+    assert mary_grades.median() == 10
+
+    "Should return the right median from an even grades list"
+    python_grades = Statistic([10, 4, 7, 6, 3, 9])
+    sleep_grades = Statistic([5, 7, 5, 3, 4, 10])
+    assert python_grades.median() == 6.5
+    assert sleep_grades.median() == 3
