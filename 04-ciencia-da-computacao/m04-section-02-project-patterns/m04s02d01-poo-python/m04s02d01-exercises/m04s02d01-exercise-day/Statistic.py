31f50db7480822a34a77effa6f0c1f5c64332b82
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-19 15:50:31 -0300

    test: method Statistic average pass case

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/Statistic.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/Statistic.py
@@ -1,6 +1,7 @@
 class Statistic:
     def __init__(self, studant_grade_list: list):
         self.studant_grade_list = studant_grade_list
+        self.__average = self.grade_average(self.studant_grade_list)
 
     @property
     def studant_grade_list(self):
@@ -13,3 +14,10 @@ class Statistic:
                 f"Should be given a list, instead receive a {type(value)}"
             )
         self.__studant_grade_list = value
+
+    @property
+    def average(self):
+        return self.__average
+
+    def grade_average(self, grade_list):
+        return sum(grade_list) / len(grade_list)
