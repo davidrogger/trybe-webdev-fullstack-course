ba3c27c2486726cbc638c55f9f80b187ad3e3ffd
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-19 13:13:25 -0300

    test: method volume_up fail case

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_Tv.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_Tv.py
@@ -16,3 +16,25 @@ def test_tv_check_size_parameter():
 def test_tv_instance_without_parameter():
     with pytest.raises(TypeError):
         Tv()
+
+
+def test_tv_method_volume_up():
+    samsung_23pol = Tv(23)
+
+    "Should exist the method volume_up"
+    callable(getattr(samsung_23pol, "volume_up"))
+
+    "Should set the volume one by one "
+    samsung_23pol.volume_up()
+    assert samsung_23pol.volume == 51
+    samsung_23pol.volume_up()
+    assert samsung_23pol.volume == 52
+
+    "Should not be possible to set above 99"
+    press_volume_up = 0
+    while press_volume_up <= 100:
+        samsung_23pol.volume_up()
+        press_volume_up += 1
+
+    assert samsung_23pol.volume == 99
+    assert samsung_23pol.volume != 152
