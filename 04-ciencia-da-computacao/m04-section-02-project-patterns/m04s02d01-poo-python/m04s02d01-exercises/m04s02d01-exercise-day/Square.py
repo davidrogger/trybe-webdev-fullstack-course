7136eafdbe2aa8063d6ce1f6f835142916081cdd
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-20 13:06:01 -0300

    test: created Square class pass case

--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/Square.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/Square.py
@@ -2,4 +2,13 @@ from GeometricFigure import GeometricFigure
 
 
 class Square(GeometricFigure):
-    pass
+    def __init__(self, side):
+        self.__side = side
+
+    def area(self):
+        base_times_height = self.__side * self.__side
+        return f"{base_times_height}m²"
+
+    def perimeter(self):
+        sum_equal_sides = self.__side * 4
+        return f"{sum_equal_sides}m²"
--- 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_GeometricFigure.py
+++ 04-ciencia-da-computacao/m04-section-02-project-patterns/m04s02d01-poo-python/m04s02d01-exercises/m04s02d01-exercise-day/test_GeometricFigure.py
@@ -4,5 +4,5 @@ from Square import Square
 def test_creating_a_square():
     square_10m = Square(10)
 
-    assert square_10m.area == "100m²"
-    assert square_10m.perimeter == "40m²"
+    assert square_10m.area() == "100m²"
+    assert square_10m.perimeter() == "40m²"
