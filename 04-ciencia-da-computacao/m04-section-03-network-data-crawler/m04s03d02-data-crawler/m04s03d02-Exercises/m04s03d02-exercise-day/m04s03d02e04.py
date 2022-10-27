19f25019286c76017642cde5491661d8daf079bc
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-27 14:33:12 -0300

    feat: m04s03d02e04 done

---
+++ 04-ciencia-da-computacao/m04-section-03-network-data-crawler/m04s03d02-data-crawler/m04s03d02-Exercises/m04s03d02-exercise-day/m04s03d02e04.py
@@ -0,0 +1,36 @@
+# 🚀 Exercício 4:
+# Baseando-se em uma página contendo detalhes sobre um livro
+# (http://books.toscrape.com/catalogue/the-grand-design_405/index.html),
+# faça a extração dos campos título, preço, descrição e url contendo a imagem
+# de capa do livro.
+
+# ⚠️ O preço deve vir somente valores numéricos e ponto. Ex: Â£13.76 -> 13.76.
+# ⚠️ A descrição de ter o sufixo “...more" removido quando existir.
+# ⚠️ Os campos extraídos devem ser apresentados separados por vírgula.
+# Ex: título,preço,descrição,capa.
+
+# Exemplo:
+# The Grand Design,13.76,THE FIRST MAJOR WORK IN NEARLY A
+# DECADE BY ONE OF THE WORLDâS GREAT THINKERSâA MARVELOUSLY
+# CONCISE BOOK WITH NEW ANSWERS TO THE ULTIMATE QUESTIONS OF
+# LIFEWhen and howdid the universe begin? Why are we here?
+# Why is there something rather than nothing? What is the nature
+# of reality? Why are the ...., http://books.toscrape.com/catalogue/../../
+# media/cache/9b/69/9b696c2064d6ee387774b6121bb4be91.jpg
+import requests
+from parsel import Selector
+
+url = "http://books.toscrape.com/catalogue/the-grand-design_405/index.html"
+
+response = requests.get(url)
+selected = Selector(response.text)
+
+title: str = selected.css("h1::text").get()
+price: str = selected.css(".price_color::text").re_first(r"£\d+\.\d{2}")
+product_detail: str = selected.css("#product_description ~ p::text").get()
+book_cover_img = selected.css(".item img::attr(src)").get()
+
+if product_detail.endswith("...more"):
+    product_detail = product_detail[: -len("...more")]
+
+print(", ".join([title, price, product_detail, book_cover_img]))
