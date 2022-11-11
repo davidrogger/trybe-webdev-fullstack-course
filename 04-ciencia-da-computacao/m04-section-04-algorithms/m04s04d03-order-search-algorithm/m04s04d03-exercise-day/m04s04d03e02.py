21bb09a1f10c6b7383e55f9ddca7ece6a6994d60
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-11 10:18:38 -0300

    feat exercise m04s04d03e02 statement

---
+++ 04-ciencia-da-computacao/m04-section-04-algorithms/m04s04d03-order-search-algorithm/m04s04d03-exercise-day/m04s04d03e02.py
@@ -0,0 +1,6 @@
+# Exercício 2 Demonstre o passo a passo do processo de
+# mistura de um array, sendo ordenado utilizando merge sort.
+# Comece o passo a passo a partir da linha abaixo:
+
+# inicio
+# 7 3    5 4    6 8    2 1
