a254c25efbe63f117e8bd2f764cf251e79e43204
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-11 11:01:54 -0300

    feat: select order analyze

--- 04-ciencia-da-computacao/m04-section-04-algorithms/m04s04d03-order-search-algorithm/m04s04d03-exercise-day/m04s04d03e03.py
+++ 04-ciencia-da-computacao/m04-section-04-algorithms/m04s04d03-order-search-algorithm/m04s04d03-exercise-day/m04s04d03e03.py
@@ -1,12 +1,45 @@
+from random import shuffle
+from Cronometro import Cronometro
+
+from selection_sort import selection_sort
+
 # Exercício 3 Execute os algoritmos de ordenação por seleção e
 # inserção, para as entradas de dados ordenadas, inversamente
 # ordenadas e aleatórias. Em seguida, compare-os. Faça testes
 # para entradas de tamanho 10.000, 100.000, 1.000.000.
 
 # ▶️ A entrada de dados pode ser gerada da seguinte maneira:
-from random import shuffle
 
-ordenados = list(range(100))
-inversamente_ordenados = list(reversed(range(100)))
+list_size = 10000
+
+ordenados = list(range(list_size))
+inversamente_ordenados = list(reversed(range(list_size)))
 aleatorios = ordenados[:]  # copia dos ordenados
 shuffle(aleatorios)  # embaralha eles
+
+
+select_operations = [
+    {
+        "type": "Seleção Ordenada",
+        "list": ordenados,
+        "method": selection_sort,
+    },
+    {
+        "type": "Seleção Inversamente Ordenada",
+        "list": inversamente_ordenados,
+        "method": selection_sort,
+    },
+    {
+        "type": "Seleção Aleatória",
+        "list": aleatorios,
+        "method": selection_sort,
+    },
+]
+
+
+for operation in select_operations:
+    name = operation["type"]
+    with Cronometro(f"{name}"):
+        method_order = operation["method"]
+        elements = operation["list"]
+        method_order(elements)
