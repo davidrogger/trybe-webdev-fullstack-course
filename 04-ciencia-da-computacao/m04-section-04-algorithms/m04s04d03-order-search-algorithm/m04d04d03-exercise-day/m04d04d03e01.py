6e23b92ce85df70f2d15f1a9bf56dd6167f817ca
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-11 10:10:59 -0300

    feat exercise m04d04d03e01 statement

---
+++ 04-ciencia-da-computacao/m04-section-04-algorithms/m04s04d03-order-search-algorithm/m04d04d03-exercise-day/m04d04d03e01.py
@@ -0,0 +1,11 @@
+from Cronometro import Cronometro
+
+# Exercício 1 Dado um array com os seguintes elementos
+# ["zebra", "macaco", "elefante", "arara", "javali"], após
+# duas iterações utilizando bubble sort, como estaria este
+# array?
+
+
+if __name__ == "__main__":
+    with Cronometro("Bubble sorte"):
+        pass
