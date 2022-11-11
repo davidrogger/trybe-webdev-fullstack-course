9792db7b1d5d7cfd8d2a16ceef185bd1e6bd1bca
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-11 10:15:10 -0300

    feat: m04d04d03e01 resolution

--- 04-ciencia-da-computacao/m04-section-04-algorithms/m04s04d03-order-search-algorithm/m04d04d03-exercise-day/m04d04d03e01.py
+++ 04-ciencia-da-computacao/m04-section-04-algorithms/m04s04d03-order-search-algorithm/m04d04d03-exercise-day/m04d04d03e01.py
@@ -1,11 +1,9 @@
-from Cronometro import Cronometro
-
 # Exercício 1 Dado um array com os seguintes elementos
 # ["zebra", "macaco", "elefante", "arara", "javali"], após
 # duas iterações utilizando bubble sort, como estaria este
 # array?
 
+original_format = ["zebra", "macaco", "elefante", "arara", "javali"]
 
-if __name__ == "__main__":
-    with Cronometro("Bubble sorte"):
-        pass
+first_iteraction = ["macaco", "elefante", "arara", "javali", "zebra"]
+second_iteraction = ["elefante", "arara", "javali", "macaco", "zebra"]
