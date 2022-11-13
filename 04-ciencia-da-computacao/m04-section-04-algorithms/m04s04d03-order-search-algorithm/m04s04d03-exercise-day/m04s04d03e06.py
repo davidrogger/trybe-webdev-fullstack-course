00ac39a8367131b4c42dee4098b1230d75581908
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-13 14:34:46 -0300

    feat: m04s04d03e06 finding the first False by binary search

--- 04-ciencia-da-computacao/m04-section-04-algorithms/m04s04d03-order-search-algorithm/m04s04d03-exercise-day/m04s04d03e06.py
+++ 04-ciencia-da-computacao/m04-section-04-algorithms/m04s04d03-order-search-algorithm/m04s04d03-exercise-day/m04s04d03e06.py
@@ -15,3 +15,45 @@
 
 # 💡 Curiosidade: O comando git bisect executa de maneira
 # similar a este exercício se implementado de forma eficiente.
+
+
+def find_index_fail(array):
+    list_size = len(array)
+    start = 0
+    end = list_size - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        until_mid = mid + 1
+
+        first = array[start:until_mid]
+
+        if False in first:
+            end = mid
+        else:
+            start = mid
+
+        if array[start] is False:
+            return start
+
+        start = start + 1
+
+    return -1
+
+
+if __name__ == "__main__":
+    example1 = [True, True, False, True, False, False, False]  # 2
+    example2 = [True, False, True, True, False, False, False]  # 1
+    example3 = [True, True, True, True, True, False, False]  # 5
+    example4 = [True, True, True, True, True, True, True]  # -1
+    example5 = [True, True, True, True, True, True, True, True, False]  # 8
+    result1 = find_index_fail(example1)
+    result2 = find_index_fail(example2)
+    result3 = find_index_fail(example3)
+    result4 = find_index_fail(example4)
+    result5 = find_index_fail(example5)
+    print(f"False Position in the first example: {result1}")
+    print(f"False Position in the first example: {result2}")
+    print(f"False Position in the first example: {result3}")
+    print(f"False Position in the first example: {result4}")
+    print(f"False Position in the first example: {result5}")
