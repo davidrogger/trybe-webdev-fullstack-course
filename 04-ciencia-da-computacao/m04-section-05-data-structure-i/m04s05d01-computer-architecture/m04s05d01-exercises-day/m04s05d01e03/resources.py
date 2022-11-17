d17d626a453f3d57b292a4432cee4419adf2b775
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-17 12:50:45 -0300

    feat: exercise m04s05d01e03 statement

---
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d01-computer-architecture/m04s05d01-exercises-day/m04s05d01e03/resources.py
@@ -0,0 +1,7 @@
+# Exercício 3: Agora vamos tanto explorar o hardware que estamos
+# utilizando quanto aprender algo interessante: enviar programaticamente
+# comandos para o shell. Crie um script chamado resources.py e utilize-o
+# para exibir no console as informações solicitadas abaixo. Para isso,
+# utilize o método check_output do módulo subprocess do Python 😎.
+
+import subprocess
