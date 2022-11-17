569bcf41a9ef3421ce85ebc8fb1478baa71cb283
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-17 16:26:17 -0300

    feat: exercise m04s05d01e05 done

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d01-computer-architecture/m04s05d01-exercises-day/m04s05d01e05/process_id.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d01-computer-architecture/m04s05d01-exercises-day/m04s05d01e05/process_id.py
@@ -3,3 +3,10 @@
 # em execução por um determinado tempo. Agora utilizando
 # os comandos de monitoramento visto no conteúdo, exiba
 # os processos em execução e então identifique o seu processo.
+from time import sleep
+from os import getpid
+
+print(getpid())
+sleep(20)
+
+# ps aux | grep <id>
