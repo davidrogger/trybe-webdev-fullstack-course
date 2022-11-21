8336868abef316fb54706dedebde66f042c5ffe7
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-21 15:16:39 -0300

    feat: exercise m04s05d02e01 my resolution

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d02-arrays/m04s05d02-exercise-day/m04s05d02e01.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d02-arrays/m04s05d02-exercise-day/m04s05d02e01.py
@@ -14,3 +14,29 @@
 
 # valores_coletados = [1, 1, 1, 1, 0, 0, 1, 1]
 # resultado = 4
+
+
+def count_instability(reports):
+    initial_time = 0
+    accumulated_time = initial_time
+    max_time = initial_time
+    for report in reports:
+        if report == 1:
+            accumulated_time += 1
+            max_time = max(max_time, accumulated_time)
+        else:
+            accumulated_time = initial_time
+
+    return max_time
+
+
+if __name__ == "__main__":
+    first = [0, 1, 1, 1, 0, 0, 1, 1]
+    second = [1, 1, 1, 1, 0, 0, 1, 1]
+
+    assert count_instability(first) == 3
+    assert count_instability(second) == 4
+
+    # Faça a análise de complexidade da sua solução.
+    # Precisa percorrer toda a lista para analisar a quantidade pelo menos
+    # uma vez, tendo complexidade linear O(n)
