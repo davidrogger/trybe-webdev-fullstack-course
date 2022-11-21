a1e038940867e22814b9c47f9e6f473e624d4cfc
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-21 08:15:33 -0300

    docs: multimensionals and others operations

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d02-arrays/m04s05d02-follow-material/array_example.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d02-arrays/m04s05d02-follow-material/array_example.py
@@ -30,19 +30,21 @@ class ListaArray:
         self.data[index] = value
 
 
-array = ListaArray()
+if __name__ == "__main__":
+    array = ListaArray()
 
-array.set(0, "Marcos")
-array.set(1, "Patrícia")
-# print(array), internamente chama o método array.__str__() que implementamos
-print(array)  # saída: ["Marcos", "Patrícia"]
+    array.set(0, "Marcos")
+    array.set(1, "Patrícia")
+    # print(array), internamente chama o método array.__str__()
+    # que implementamos
+    print(array)  # saída: ["Marcos", "Patrícia"]
 
-# inserindo no começo do array
-array.set(0, "Valeria")
-print(array)  # saída: ["Valeria", "Marcos", "Patrícia"]
+    # inserindo no começo do array
+    array.set(0, "Valeria")
+    print(array)  # saída: ["Valeria", "Marcos", "Patrícia"]
 
-array.update(0, "Jonas")
-print(array)
+    array.update(0, "Jonas")
+    print(array)
 
-array_memory_size = sys.getsizeof(array.data)
-print(array_memory_size)
+    array_memory_size = sys.getsizeof(array.data)
+    print(array_memory_size)
