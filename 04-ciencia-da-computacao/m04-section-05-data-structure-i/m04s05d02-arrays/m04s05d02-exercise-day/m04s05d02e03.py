a540a7fba2971a78bdd67b559c6f9b01e915670c
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-21 16:31:43 -0300

    feat: exercise m04s05d02e03 resolution

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d02-arrays/m04s05d02-exercise-day/m04s05d02e03.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d02-arrays/m04s05d02-exercise-day/m04s05d02e03.py
@@ -7,6 +7,7 @@
 
 # Exemplo 1:
 # produtos = [1, 3, 1, 1, 2, 3]
+#             0, 1, 2, 3, 4, 5
 # resultado = 4
 # Os índices (0, 2), (0, 3), (1, 5), (2, 3) formam combinações.
 
@@ -14,3 +15,28 @@
 # produtos = [1, 1, 2, 3]
 # resultado = 1
 # Os índices (0, 1) formam a única combinação.
+
+
+def combined_product(products):
+    match_products = []
+    products_size = len(products)
+
+    for current_index in range(products_size):
+        right_elements = current_index + 1
+        for next_index in range(right_elements, products_size):
+            if products[current_index] == products[next_index]:
+                match_products.append((current_index, next_index))
+
+    return len(match_products)
+
+
+if __name__ == "__main__":
+    first = [1, 3, 1, 1, 2, 3]
+    second = [1, 1, 2, 3]
+
+    assert combined_product(first) == 4
+    assert combined_product(second) == 1
+    # Faça a análise de complexidade da sua solução.
+    # Por percorrer a lista dentro da outra para comparar mais de uma
+    # vez os elementos dobrando o tamanho da busca, tornando a
+    # complexidade de tempo quadratica O(n^2)
