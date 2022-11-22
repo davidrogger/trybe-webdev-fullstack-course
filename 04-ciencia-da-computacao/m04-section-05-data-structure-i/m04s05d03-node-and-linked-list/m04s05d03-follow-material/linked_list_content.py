c476bfe36ead7dc296db827bf73db3e134f95bac
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-22 17:18:06 -0300

    feat: m04s05d03e01 done

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d03-node-and-linked-list/m04s05d03-follow-material/linked_list_content.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d03-node-and-linked-list/m04s05d03-follow-material/linked_list_content.py
@@ -20,13 +20,12 @@ class LinkedList:
 
     def insert_last(self, value):
         last_value = Node(value)
-        current_value = self.head_value
+        last_position = self.__length - 1
+        current_value = self.__get_node_at(last_position)
 
         if current_value is None:
             return self.insert_first(value)
 
-        while current_value.next:
-            current_value = current_value.next
         current_value.next = last_value
         self.__length += 1
 
@@ -35,10 +34,9 @@ class LinkedList:
             return self.insert_first(value)
         if position >= len(self):
             return self.insert_last(value)
-        current_value = self.head_value
-        while position > 1:
-            current_value = current_value.next
-            position -= 1
+
+        current_value = self.__get_node_at(position - 1)
+
         next_value = Node(value)
         next_value.next = current_value.next
         current_value.next = next_value
@@ -55,11 +53,8 @@ class LinkedList:
     def remove_last(self):
         if len(self) <= 1:
             return self.remove_first()
-
-        previous_to_be_removed = self.head_value
-
-        while previous_to_be_removed.next.next:
-            previous_to_be_removed = previous_to_be_removed.next
+        last_position = self.__length - 1
+        previous_to_be_removed = self.__get_node_at(last_position - 1)
 
         value_to_be_removed = previous_to_be_removed.next
         previous_to_be_removed.next = None
@@ -72,10 +67,8 @@ class LinkedList:
         if position >= len(self):
             return self.remove_last()
 
-        previous_to_be_removed = self.head_value
-        while position > 1:
-            previous_to_be_removed = previous_to_be_removed.next
-            position -= 1
+        previous_to_be_removed = self.__get_node_at(position - 1)
+
         value_to_be_removed = previous_to_be_removed.next
         previous_to_be_removed.next = value_to_be_removed.next
         value_to_be_removed.next = None
@@ -84,46 +77,34 @@ class LinkedList:
 
     def get_element_at(self, position):
         value_returned = None
+        value_to_be_returned = self.__get_node_at(position)
+        if value_to_be_returned:
+            value_returned = Node(value_to_be_returned.value)
+            return value_returned
+
+    def __get_node_at(self, position):
         value_to_be_returned = self.head_value
         if value_to_be_returned:
             while position > 0 and value_to_be_returned.next:
                 value_to_be_returned = value_to_be_returned.next
                 position -= 1
-            if value_to_be_returned:
-                value_returned = Node(value_to_be_returned.value)
-            return value_returned
+        return value_to_be_returned
 
     def is_empty(self):
         return not self.__length
+
+    def clear(self):
+        while not self.is_empty():
+            self.remove_first()
 
 
 if __name__ == "__main__":
     linked_list = LinkedList()
 
-    print(linked_list.is_empty())
-    linked_list.insert_first(1)
-    print(linked_list)
-
-    linked_list.insert_first(2)
-    print(linked_list)
-
-    linked_list.insert_last(3)
-    print(linked_list)
+    linked_list.insert_first(1)  # 4
+    linked_list.insert_first("David")  # 3
+    linked_list.insert_first("Rogger")  # 2
+    linked_list.insert_first("teste")  # 1
+    linked_list.insert_first(15)  # 0
 
     linked_list.remove_last()
-    print(linked_list)
-
-    linked_list.remove_first()
-    print(linked_list)
-
-    linked_list.insert_at(5, 1)
-    print(linked_list)
-
-    linked_list.remove_at(0)
-    print(linked_list)
-
-    linked_list.insert_at(6, 1)
-    linked_list.insert_at(7, 2)
-    linked_list.insert_at(8, 3)
-    linked_list.insert_at(9, 4)
-    print(linked_list.get_element_at(3))
