3261051e497fff503fae6107327220fbe9f58d29
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-24 11:05:25 -0300

    feat: added get head and tail

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/linked_list.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/linked_list.py
@@ -3,40 +3,43 @@ from node import Node
 
 class LinkedList:
     def __init__(self):
-        self.head = Node("HEAD")
-        self.tail = self.head
-        self.head.next = None
+        self.__head = Node("HEAD")
+        self.__tail = self.__head
+        self.__head.next = None
         self.__length = 0
 
     def __len__(self):
         return self.__length
 
     def __repr__(self):
-        return f"LinkedList:(len={self.__length}, value={self.head})"
+        return f"LinkedList:(len={self.__length}, value={self.__head})"
 
     def is_empty(self):
         return not self.__length
 
     def insert_first(self, value):
-        old_header = self.head
-        self.head = Node(value)
+        old_header = self.__head
+        self.__head = Node(value)
 
         if not self.is_empty():
-            self.head.next = old_header
+            self.__head.next = old_header
 
-        current_element = self.head
+        if self.is_empty():
+            self.__tail = self.__head
 
-        while current_element.next:
-            current_element = current_element.next
-
-        self.tail = current_element
         self.__length += 1
 
     def insert_last(self, value):
         if self.is_empty():
             self.insert_first(value)
 
-        self.tail.next = Node(value)
-        self.tail = self.tail.next
+        self.__tail.next = Node(value)
+        self.__tail = self.__tail.next
 
         self.__length += 1
+
+    def get_head(self):
+        return self.__head.value
+
+    def get_tail(self):
+        return self.__tail.value
