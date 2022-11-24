94199073e36592ade57ecb61465aa7634bee7403
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-24 09:23:19 -0300

    feat: exercise m04s05d04e01 statment

---
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/m04s05d04e01.py
@@ -0,0 +1,7 @@
+# 🚀 Exercício 1:
+# Filas: crie uma classe Queue, onde deve conter as operações:
+# enqueue, dequeue, peek e is_empty.
+
+# Para este desafio, é necessário efetuar o import das classes
+# LinkedList e Node e utilizar seus métodos de acesso para simular
+# uma fila, respeitando o padrão FIFO.
