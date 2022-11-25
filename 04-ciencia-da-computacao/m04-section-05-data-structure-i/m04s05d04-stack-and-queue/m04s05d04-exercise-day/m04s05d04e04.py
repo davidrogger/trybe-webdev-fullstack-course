31a419f0ef46c5ee2bfdd52f5d6e8584b0c50f68
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-25 15:41:59 -0300

    docs: m04s05d04e04 statement

---
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/m04s05d04e04.py
@@ -0,0 +1,16 @@
+# Exercício 4:
+# Estenda a classe Stack, que escrevemos durante as explicações do
+# conteúdo, para que ela suporte um limite de itens dentro da pilha.
+# Se definirmos que a pilha deve ter o tamanho dois, ela não poderá
+# ter três itens. Por exemplo:
+
+# ...
+
+# content_stack = LimitStack(2)
+# content_stack.push(1)
+# content_stack.push(-2)
+
+# try:
+#     content_stack.push(3)
+# except StackOverflow:
+#     print("Não é possível adicionar outro item à pilha")
