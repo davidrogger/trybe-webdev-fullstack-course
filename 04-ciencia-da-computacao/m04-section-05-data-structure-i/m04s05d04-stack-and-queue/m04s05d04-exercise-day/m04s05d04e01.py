0986e9066f12cc92aa29619f2e4015550404f288
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-25 11:38:55 -0300

    feat: initial structure Queue

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/m04s05d04e01.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/m04s05d04e01.py
@@ -1,3 +1,5 @@
+from linked_list import LinkedList
+
 # 🚀 Exercício 1:
 # Filas: crie uma classe Queue, onde deve conter as operações:
 # enqueue, dequeue, peek e is_empty.
@@ -5,3 +7,20 @@
 # Para este desafio, é necessário efetuar o import das classes
 # LinkedList e Node e utilizar seus métodos de acesso para simular
 # uma fila, respeitando o padrão FIFO.
+
+
+class Queue:
+    def __init__(self):
+        self.queue = LinkedList()
+
+    def enqueue(self):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def is_empty(self):
+        pass
---
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/test_queue.py
@@ -0,0 +1,17 @@
+from m04s05d04e01 import Queue
+
+
+def test_enqueue():
+    pass
+
+
+def test_dequeue():
+    pass
+
+
+def test_peek():
+    pass
+
+
+def test_is_empty():
+    pass
