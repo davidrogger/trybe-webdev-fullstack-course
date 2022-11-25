ed22e0740f534a0c30134c7922c2c2848926313a
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-25 15:15:40 -0300

    docs: m04s05d04e03 statement

---
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/m04s05d04e03.py
@@ -0,0 +1,11 @@
+# Exercício 3:
+# Estenda a classe Stack, que escrevemos durante as explicações do
+# conteúdo, adicionando uma nova função chamada min_value() que irá
+# retornar o menor valor inteiro presente na pilha. Por exemplo:
+
+# # ...
+
+# content_stack.push(1)
+# content_stack.push(-2)
+# content_stack.push(3)
+# print(content_stack.min_value()) # saída: -2
