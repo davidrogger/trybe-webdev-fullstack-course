a960af6df770dcc8fc23d6b1941b6756170d46e2
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-27 11:29:27 -0300

    docs: m04s05d04e07 statement

---
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/m04s05d04e07.py
@@ -0,0 +1,8 @@
+# Exercício 7:
+# Dado uma string, contendo letras e parênteses.
+# Crie uma função que irá reverter os caracteres de tal forma
+# que somente os caracteres dentro dos parênteses sejam revertidos.
+# A string final não deve conter os parênteses. Por exemplo:
+
+# string = 'teste(lagel)'
+# resultado = 'testelegal'
