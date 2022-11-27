b370f70e233c86a24faec2d110cc230f61474d3b
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-27 11:07:57 -0300

    docs: m04s05d04e06 statement

---
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d04-stack-and-queue/m04s05d04-exercise-day/m04s05d04e06.py
@@ -0,0 +1,7 @@
+# Exercício 6:
+# Dada a função que faz a resolução de expressões pós fixas, adicione
+# as operações de subtração e divisão.
+# Considere os exemplos abaixo para testar a sua alteração na função.
+
+# Nota: transforme as expressões em pós fixas e atribua valores.
+# Caso seja necessário, faça o cast do valor para ponto flutuante.
