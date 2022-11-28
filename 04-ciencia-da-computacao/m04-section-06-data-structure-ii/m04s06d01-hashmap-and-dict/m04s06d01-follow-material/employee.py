1e571ce5f930d3ac247b893fd0e8837c35dacad6
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-28 09:56:46 -0300

    feat: new class Hashmap

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/m04s06d01-hashmap-and-dict/m04s06d01-follow-material/employee.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/m04s06d01-hashmap-and-dict/m04s06d01-follow-material/employee.py
@@ -2,3 +2,11 @@ class Employee:
     def __init__(self, id_num, name):
         self.id_num = id_num
         self.name = name
+
+
+class HashMap:
+    def __init__(self):
+        self._buckets = [None for _ in range(10)]
+
+    def get_address(self, id_num):
+        return id_num % 10
