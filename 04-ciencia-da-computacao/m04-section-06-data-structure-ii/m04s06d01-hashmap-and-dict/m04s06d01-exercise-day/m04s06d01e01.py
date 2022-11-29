b0db559c7126376022b1d104b455ddf3bef05f8e
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-29 08:12:23 -0300

    docs: exercise 01 statement

---
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/m04s06d01-hashmap-and-dict/m04s06d01-exercise-day/m04s06d01e01.py
@@ -0,0 +1,15 @@
+# 🚀 Exercício 1 - Facebook
+# Você receberá uma lista de palavras e uma string. Escreva uma
+# função que decida quais palavras podem ser formadas com os caracteres
+# da string (cada caractere só pode ser utilizado uma vez). Retorne a soma
+# do comprimento das palavras escolhidas.
+
+# words = ["cat", "bt", "hat", "tree"], chars = "atach"
+# # saída: 6
+# """Explicação: As palavras que podem ser formadas com os caracteres
+# da string são "cat" (tamanho 3) e "hat" (tamanho 3)."""
+
+# words = ["hello", "world", "students"], chars = "welldonehoneyr"
+# # saída: 10
+# """Explicação: As palavras que podem ser formadas com os caracteres
+# da string são "hello" (tamanho 5) e "world" (tamanho 5)."""
