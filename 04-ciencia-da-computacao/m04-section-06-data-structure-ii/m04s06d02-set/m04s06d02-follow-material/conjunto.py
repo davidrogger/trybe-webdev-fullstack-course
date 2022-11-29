fe4ad59902b9a67e3f770515b00f0f9f9e954480
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-29 14:39:41 -0300

    feat: practice added method intersection

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/m04s06d02-set/m04s06d02-follow-material/conjunto.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/m04s06d02-set/m04s06d02-follow-material/conjunto.py
@@ -15,6 +15,9 @@ class Conjunto:
 
         length = len(elements)
 
+        if length == 0:
+            return "{}"
+
         str_elements = "{"
 
         for position, element in enumerate(elements, start=1):
@@ -48,6 +51,20 @@ class Conjunto:
 
         return new_set
 
+    def intersection(self, set):
+        matched_elements = []
+
+        for position in range(len(set)):
+            if set[position] and self.elements[position]:
+                matched_elements.append(position)
+
+        new_set = Conjunto()
+
+        for position in matched_elements:
+            new_set.add(position)
+
+        return new_set
+
 
 if __name__ == "__main__":
     elements = [0, 10, 100, 1000]
@@ -71,3 +88,29 @@ if __name__ == "__main__":
 
     print(conjunto.union(conjuntoB))  # {0, 10, 20, 30, 50, 100, 1000}
     print(conjunto)  # {0, 10, 100, 1000}
+
+    conjuntoC = Conjunto()
+    conjuntoD = Conjunto()
+
+    elementsC = [1, 2, 3]
+    for element in elementsC:
+        conjuntoC.add(element)
+
+    elementsD = [3, 4, 5]
+    for element in elementsD:
+        conjuntoD.add(element)
+
+    print(conjuntoC.intersection(conjuntoD))  # {3}
+
+    conjuntoE = Conjunto()
+    conjuntoF = Conjunto()
+
+    elementsE = [0, 3, 6, 9]
+    for element in elementsE:
+        conjuntoE.add(element)
+
+    elementsF = [12, 15, 18]
+    for element in elementsF:
+        conjuntoF.add(element)
+
+    print(conjuntoE.intersection(conjuntoF))  # {}
