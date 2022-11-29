8a2e50de33647ccfd4d75b9d2265203a5f1b0458
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-29 16:05:09 -0300

    feat: added analyze set2

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/m04s06d02-set/m04s06d02-follow-material/conjunto.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/m04s06d02-set/m04s06d02-follow-material/conjunto.py
@@ -65,6 +65,92 @@ class Conjunto:
 
         return new_set
 
+    def difference(self, set):
+        matched_elements = []
+
+        for position in range(1001):
+            if set[position] and not self.elements[position]:
+                matched_elements.append(position)
+            elif not set[position] and self.elements[position]:
+                matched_elements.append(position)
+
+        new_set = Conjunto()
+
+        for position in matched_elements:
+            new_set.add(position)
+
+        return new_set
+
+
+# GABARITO
+class Conjunto2:
+    def __init__(self):
+        self.set = [False] * 1001
+        self.last_element = 0
+
+    def __str__(self):
+        string = "{"
+
+        for index, is_in_set in enumerate(self.set):
+            if is_in_set:
+                string += str(index)
+                if index < self.last_element:
+                    string += ", "
+
+        string += "}"
+
+        return string
+
+    def __contains__(self, item):
+        return self.set[item]
+
+    def add(self, item):
+        if not self.set[item]:
+            self.set[item] = True
+        if item > self.last_element:
+            self.last_element = item
+
+    def union(self, conjuntoB):
+        new_conjunto = Conjunto2()
+
+        for index in range(1001):
+            if self.set[index] or conjuntoB.set[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
+    def intersection(self, conjuntoB):
+        new_conjunto = Conjunto2()
+
+        for index in range(1001):
+            if self.set[index] and conjuntoB.set[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
+    def difference(self, conjuntoB):
+        new_conjunto = Conjunto2()
+
+        for index in range(1001):
+            if self.set[index] and not conjuntoB.set[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
+    def issubset(self, conjuntoB):
+        for index in range(1001):
+            if self.set[index] and not conjuntoB.set[index]:
+                return False
+
+        return True
+
+    def issuperset(self, conjuntoB):
+        for index in range(1001):
+            if conjuntoB.set[index] and not self.set[index]:
+                return False
+
+        return True
+
 
 if __name__ == "__main__":
     elements = [0, 10, 100, 1000]
@@ -101,6 +187,7 @@ if __name__ == "__main__":
         conjuntoD.add(element)
 
     print(conjuntoC.intersection(conjuntoD))  # {3}
+    print(conjuntoC.difference(conjuntoD))  # {1, 2, 4, 5}
 
     conjuntoE = Conjunto()
     conjuntoF = Conjunto()
