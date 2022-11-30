6bff970e95b09d341e41f06fe235c246db45ae9e
Davíd Roggér <davidrogger.a@gmail.com>
2022-11-30 14:20:14 -0300

    test: dequeue

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d05-ting/tests/test_queue.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d05-ting/tests/test_queue.py
@@ -1,4 +1,16 @@
 from file_management.Queue import Queue
+import pytest
+
+
+@pytest.fixture
+def queue():
+    queue = Queue()
+    items = [1, 2, 10, 5, 30, 60]
+
+    for item in items:
+        queue.enqueue(item)
+
+    return queue
 
 
 def test_queue_create():
@@ -19,3 +31,16 @@ def test_enqueue_elements():
         queue.enqueue(item)
 
     assert len(queue) == 5
+
+
+def test_dequeue_elements(queue: Queue):
+    first_length_expect = 5
+    second_length_expect = 0
+    queue.dequeue()
+
+    assert len(queue) == first_length_expect
+
+    for _ in range(first_length_expect):
+        queue.dequeue()
+
+    assert len(queue) == second_length_expect
