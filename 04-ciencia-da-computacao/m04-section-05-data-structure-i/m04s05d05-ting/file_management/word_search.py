dbc0fb1636016285b153feff92c7e7f253cd5d65
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-01 10:57:47 -0300

    feat: exists word

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d05-ting/file_management/word_search.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d05-ting/file_management/word_search.py
@@ -2,4 +2,25 @@ from file_management.Queue import Queue
 
 
 def exists_word(search_word: str, instance: Queue):
-    pass
+    uncapitalize_word = search_word.lower()
+    report = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        words_found_at = []
+        path_file = file["nome_do_arquivo"]
+        lines = file["linhas_do_arquivo"]
+
+        for line, phrase in enumerate(lines, start=1):
+            uncapitalize_phrase = phrase.lower()
+            if uncapitalize_word in uncapitalize_phrase:
+                words_found_at.append({"linha": line})
+        if len(words_found_at):
+            data_found = {
+                "palavra": search_word,
+                "arquivo": path_file,
+                "ocorrencias": words_found_at,
+            }
+            report.append(data_found)
+
+    return report
