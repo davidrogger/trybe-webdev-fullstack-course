b2affac87b8763bd5aa3a3689531cffc2f6e0796
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-01 08:04:04 -0300

    test: remove element from queue

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d05-ting/tests/test_file_process.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d05-ting/tests/test_file_process.py
@@ -1,14 +1,15 @@
-from file_management.file_process import file_process
+from file_management.file_process import process, remove
 from file_management.Queue import Queue
+import pytest
 
 
 def test_file_process(capsys):
     new_queue = Queue()
-    file_process("mock_file/test_file.txt", new_queue)
+    process("mock_file/test_file.txt", new_queue)
     out, _ = capsys.readouterr()
     expect = str(
         {
-            "nome_do_arquivo": "test_file.txt",
+            "nome_do_arquivo": "mock_file/test_file.txt",
             "qtd_linhas": 3,
             "linhas_do_arquivo": [
                 "First line\n",
@@ -24,6 +25,35 @@ def test_file_process(capsys):
 def test_file_process_ignore_equal_file_names():
     new_queue = Queue()
     expect_length = 1
-    file_process("mock_file/test_file.txt", new_queue)
-    file_process("mock_file/test_file.txt", new_queue)
+    process("mock_file/test_file.txt", new_queue)
+    process("mock_file/test_file.txt", new_queue)
     assert len(new_queue) == expect_length
+
+
+@pytest.fixture
+def queue_populated():
+    new_queue = Queue()
+    process("mock_file/test_file.txt", new_queue)
+    return new_queue
+
+
+def test_remove_having_an_element(queue_populated, capsys):
+    expect = "Arquivo mock_file/test_file.txt removido com sucesso"
+    assert len(queue_populated) == 1
+
+    remove(queue_populated)
+    out, _ = capsys.readouterr()
+
+    assert len(queue_populated) == 0
+    assert out == expect
+
+
+def test_remove_empty(capsys):
+    expect = "Não há elementos"
+    new_queue = Queue()
+
+    remove(new_queue)
+
+    out, _ = capsys.readouterr()
+
+    assert out == expect
