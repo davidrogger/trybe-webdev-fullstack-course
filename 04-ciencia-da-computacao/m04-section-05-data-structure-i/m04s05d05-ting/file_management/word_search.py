8f66c15ef3edee0fc34cb62f76c915f6f4272578
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-01 11:47:50 -0300

    wip: search by word

--- 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d05-ting/file_management/word_search.py
+++ 04-ciencia-da-computacao/m04-section-05-data-structure-i/m04s05d05-ting/file_management/word_search.py
@@ -1,6 +1,25 @@
 from file_management.Queue import Queue
 
 
+def find_word_line(uncapitalize_word: str, lines: list):
+    words_found = []
+    for line, phrase in enumerate(lines, start=1):
+        uncapitalize_phrase = phrase.lower()
+        if uncapitalize_word in uncapitalize_phrase:
+            words_found.append({"linha": line})
+    return words_found
+
+
+def look_at_instance(uncapitalize_word: str, instance: Queue):
+    for index in range(len(instance)):
+        file = instance.search(index)
+        path_file = file["nome_do_arquivo"]
+        lines = file["linhas_do_arquivo"]
+        words_found = find_word_line(uncapitalize_word, lines)
+
+        return path_file, words_found
+
+
 def exists_word(search_word: str, instance: Queue):
     uncapitalize_word = search_word.lower()
     report = []
@@ -15,6 +34,7 @@ def exists_word(search_word: str, instance: Queue):
             uncapitalize_phrase = phrase.lower()
             if uncapitalize_word in uncapitalize_phrase:
                 words_found_at.append({"linha": line})
+
         if len(words_found_at):
             data_found = {
                 "palavra": search_word,
@@ -27,4 +47,11 @@ def exists_word(search_word: str, instance: Queue):
 
 
 def search_by_word(search_word: str, instance: Queue):
-    pass
+    files_data = exists_word(search_by_word, instance)
+
+    for data in files_data:
+        for line in data["ocorrencias"]["linha"]:
+            index_queue = line - 1
+            instance.search(index_queue)
+
+    return
