99357a15633dc7bc3209391be38bf883a833ca4d
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-04 06:28:25 -0300

    feat: get most ordered by

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/analyze_log.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/analyze_log.py
@@ -25,11 +25,23 @@ def get_clients_data(orders_data):
     return clients_data
 
 
-def get_qt_product_order_by(name, product, source):
-    pass
+def get_most_ordered_by(name, source):
+    first_item = 0
+    client_orders = source[name]["orders"]
+
+    products_order = [*client_orders.keys()]
+    most_ordered = products_order[first_item]
+
+    for index in range(1, len(products_order)):
+        if client_orders[most_ordered] < client_orders[products_order[index]]:
+            most_ordered = client_orders[products_order[index]]
+
+    return most_ordered
 
 
 def analyze_log(path_to_file):
     orders = get_orders(path_to_file)
 
     clients_data = get_clients_data(orders)
+
+    maria_most_ordered = get_most_ordered_by("maria", clients_data)
