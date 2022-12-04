c967f6e43b7efcb91a0c1e4409899ecb565d240f
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-04 06:27:54 -0300

    feat: get clients data

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/analyze_log.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/analyze_log.py
@@ -9,5 +9,27 @@ def get_orders(path_file):
     return orders
 
 
+def get_clients_data(orders_data):
+    clients_data = dict()
+    for name, order, day in orders_data:
+        if name not in clients_data:
+            clients_data[name] = {"orders": {}, "days": set()}
+
+        if order not in clients_data[name]["orders"]:
+            clients_data[name]["orders"][order] = 1
+        else:
+            clients_data[name]["orders"][order] += 1
+
+        clients_data[name]["days"].add(day)
+
+    return clients_data
+
+
+def get_qt_product_order_by(name, product, source):
+    pass
+
+
 def analyze_log(path_to_file):
     orders = get_orders(path_to_file)
+
+    clients_data = get_clients_data(orders)
