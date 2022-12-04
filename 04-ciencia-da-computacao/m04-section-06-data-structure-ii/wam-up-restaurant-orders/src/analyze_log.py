12dac15f16c3319cfaf45853536fe82fed42c175
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-04 06:27:07 -0300

    feat: get orders by file

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/analyze_log.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/analyze_log.py
@@ -1,2 +1,13 @@
+import csv
+
+
+def get_orders(path_file):
+    with open(path_file, "r") as file:
+        file_data = csv.reader(file)
+        orders = [*file_data]
+
+    return orders
+
+
 def analyze_log(path_to_file):
-    pass
+    orders = get_orders(path_to_file)
