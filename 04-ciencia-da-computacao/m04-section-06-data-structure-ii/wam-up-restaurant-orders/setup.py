7c1982ed515ead84a50c13c8311e6dd378f78988
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-05 09:17:36 -0300

    chore: added configuration files

---
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="restaurant_orders",
+    description="Warm up Project Restaurant Orders",
+    install_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
---
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/inventory_control.py
@@ -0,0 +1,2 @@
+class InventoryControl:
+    pass
