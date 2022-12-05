222050a7a7b3baa736b253e9551307f12481151c
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-05 09:43:43 -0300

    test: most ordered dish per costumer

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/tests/test_trackorders.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/tests/test_trackorders.py
@@ -11,3 +11,16 @@ def test_add_new_order():
     track_orders = TrackOrders()
     track_orders.add_new_order("Jonas", "misto-quente", "segunda-feira")
     assert len(track_orders) == 1
+
+
+def test_get_most_ordered_dish_per_costumer():
+    track_orders = TrackOrders()
+    track_orders.add_new_order("Jonas", "coxinha", "segunda-feira")
+    track_orders.add_new_order("Jonas", "pizza", "terça-feira")
+    track_orders.add_new_order("Jonas", "coxinha", "quarta-feira")
+    track_orders.add_new_order("Jonas", "hamburguer", "sexta-feira")
+
+    expect = "coxinha"
+    most_ordered = track_orders.get_most_ordered_dish_per_costumer("Jonas")
+
+    assert most_ordered == expect
