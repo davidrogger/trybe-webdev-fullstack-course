31346ce2fa29d5723dc7eace7b57f4121dfe18a3
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-05 11:17:59 -0300

    feat: added counter when adding a new order
    to avoid more complexity of time, created a day counter when creating a new order, and created tow methods to identify what day has more moviments

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/track_orders.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/track_orders.py
@@ -5,10 +5,41 @@ class TrackOrders:
     def __init__(self) -> None:
         self.orders = dict()
         self.menu = set()
-        self.open_days = set()
+        self.open_days = dict()
+        self.busiest_day = None
+        self.less_busy_day = None
 
     def __len__(self):
         return len(self.orders)
+
+    def update_busiest_day(self, day):
+        if not self.busiest_day:
+            self.busiest_day = day
+        else:
+            busiest_day_moviment = self.open_days[self.busiest_day]
+            day_moviment = self.open_days[day]
+
+            if day_moviment > busiest_day_moviment:
+                self.busiest_day = day
+
+    def update_less_busy_day(self, day):
+        if not self.less_busy_day:
+            self.less_busy_day = day
+        else:
+            less_busy_day_moviment = self.open_days[self.less_busy_day]
+            day_moviment = self.open_days[day]
+
+            if day_moviment < less_busy_day_moviment:
+                self.less_busy_day = day
+
+    def add_movement_day(self, day):
+        if day not in self.open_days:
+            self.open_days[day] = 1
+        else:
+            self.open_days[day] += 1
+
+        self.update_busiest_day(day)
+        self.update_less_busy_day(day)
 
     def add_new_order(self, costumer, order, day):
         if costumer not in self.orders:
@@ -17,7 +48,7 @@ class TrackOrders:
             self.orders[costumer]["days"] = set()
 
         self.menu.add(order)
-        self.open_days.add(day)
+        self.add_movement_day(day)
 
         self.orders[costumer]["orders"].append(order)
         self.orders[costumer]["days"].add(day)
@@ -39,11 +70,12 @@ class TrackOrders:
 
     def get_days_never_visited_per_costumer(self, costumer):
         visited_days = self.orders[costumer]["days"]
+        open_days = set(self.open_days)
 
-        return self.open_days.difference(visited_days)
+        return open_days.difference(visited_days)
 
     def get_busiest_day(self):
-        pass
+        return self.busiest_day
 
     def get_least_busy_day(self):
-        pass
+        return self.less_busy_day
