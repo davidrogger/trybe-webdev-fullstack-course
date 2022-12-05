e41dce91e4092e49b5e6f245f354ad857303d644
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-05 09:23:35 -0300

    test: added new order to trackorders

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/tests/test_trackorders.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/tests/test_trackorders.py
@@ -5,3 +5,9 @@ def test_start_length_track():
     track_orders = TrackOrders()
 
     assert len(track_orders) == 0
+
+
+def test_add_new_order():
+    track_orders = TrackOrders()
+    track_orders.add_new_order("Jonas", "misto-quente", "segunda-feira")
+    assert len(track_orders) == 1
