ad6d58661e9d606f957db3ea552a56bd53fd2f7a
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-06 08:44:33 -0300

    feat: get quantities to buy

--- 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/inventory_control.py
+++ 04-ciencia-da-computacao/m04-section-06-data-structure-ii/wam-up-restaurant-orders/src/inventory_control.py
@@ -1,3 +1,6 @@
+from src.track_orders import TrackOrders
+
+
 class InventoryControl:
     INGREDIENTS = {
         "hamburguer": ["pao", "carne", "queijo"],
@@ -17,11 +20,34 @@ class InventoryControl:
         "frango": 50,
     }
 
+    CURRENT_INVENTORY = {
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
+    }
+
     def __init__(self):
-        pass
+        self.orders = TrackOrders()
+
+    def update_current_inventory(self, order):
+        for ingredient in self.INGREDIENTS[order]:
+            self.CURRENT_INVENTORY[ingredient] -= 1
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.add_new_order(costumer, order, day)
+        self.update_current_inventory(order)
 
     def get_quantities_to_buy(self):
-        pass
+        quantities_to_buy = dict()
+
+        for ingredient in self.MINIMUM_INVENTORY:
+            current_quantity = self.CURRENT_INVENTORY[ingredient]
+            minimum_quantity = self.MINIMUM_INVENTORY[ingredient]
+
+            quantities_to_buy[ingredient] = minimum_quantity - current_quantity
+
+        return quantities_to_buy
