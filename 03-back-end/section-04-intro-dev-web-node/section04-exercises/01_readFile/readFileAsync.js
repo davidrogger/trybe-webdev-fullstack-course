c6c22678e6dc6a3a05cace0f751d44a90e984327
davidrogger <davidrogger.a@gmail.com>
2023-06-06 14:47:24 -0300

    FEAT: reading file with fs

---
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/01_readFile/readFileAsync.js
@@ -0,0 +1,21 @@
+const fs = require('fs/promises');
+
+const fileData = {
+  path: './myFile.txt',
+  code: 'utf8'
+}
+
+fs.readFile(fileData.path, fileData.code)
+  .then(response => console.log(response))
+  .catch(e => console.error('Error: %s', e.path));
+
+// ou
+
+async function main() {
+  try {
+    const data = await fs.readFile(fileData.path, fileData.code);
+    console.log(data);
+  } catch (e) {
+    console.error('Error: %s', e.path);
+  }
+}
