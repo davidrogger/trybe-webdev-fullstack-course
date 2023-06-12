d3610b7bbb2e9e64a5ee523e97e24e095f0649af
davidrogger <davidrogger.a@gmail.com>
2023-06-12 14:41:30 -0300

    CHORE: added route POST new team

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/09_soccer-team-manager/src/app.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/09_soccer-team-manager/src/app.js
@@ -3,8 +3,16 @@ const teams = require('./assets/teams');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/teams', (_, res) => {
   res.status(200).json(teams);
+});
+
+app.post('/teams', (req, res) => {
+  const payload = { ...req.body };
+  teams.push(payload);
+  res.status(201).json({ team: payload });
 });
 
 module.exports = app;
