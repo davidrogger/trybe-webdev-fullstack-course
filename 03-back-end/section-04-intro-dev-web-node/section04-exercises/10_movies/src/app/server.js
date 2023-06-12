992206658d6076937e5e762764c81bdf23cabaaa
davidrogger <davidrogger.a@gmail.com>
2023-06-12 16:14:37 -0300

    FEAT: added url path by env

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/10_movies/src/app/server.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/10_movies/src/app/server.js
@@ -1,10 +1,12 @@
 const app = require('./app');
 require('dotenv').config();
 
-const { API_PORT } = process.env;
+const { API_URL, API_PORT } = process.env;
 
-if (API_PORT) {
-  app.listen(API_PORT, () => console.log('Listening at port %s', API_PORT));
-} else {
+if (!API_PORT) {
   throw new Error('Something went worng with the environment port');
+} else if (!API_URL) {
+  throw new Error('Something went worng with the environment url');
+} else {
+  app.listen(API_PORT, API_URL, () => console.log('Running application at %s:%s', API_URL, API_PORT));
 }
