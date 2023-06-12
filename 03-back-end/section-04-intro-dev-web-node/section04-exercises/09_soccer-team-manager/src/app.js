415c96d43e7455b4534a1264b281047b7b77e960
davidrogger <davidrogger.a@gmail.com>
2023-06-12 14:09:17 -0300

    CHORE: raw answer teams

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/09_soccer-team-manager/src/app.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/09_soccer-team-manager/src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const teams = require('./assets/teams');
 
 const app = express();
+
+app.get('/teams', (_, res) => {
+  res.status(200).json(teams);
+});
 
 module.exports = app;
