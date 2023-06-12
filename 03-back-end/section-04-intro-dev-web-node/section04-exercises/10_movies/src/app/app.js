0db44dafb8e18805abacbc912fd643a4123986f9
davidrogger <davidrogger.a@gmail.com>
2023-06-12 16:15:14 -0300

    FEAT: get movies from file json

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/10_movies/src/app/app.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/10_movies/src/app/app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const { getAllMovies } = require('../assets/fsHandler');
 
 const app = express();
+
+app.get('/status', (_, res) => {
+  res.status(200).json({ message: 'online' });
+});
+
+app.get('/movies', async (_, res) => {
+  const movies = await getAllMovies();
+  res.status(200).json(movies);
+});
 
 module.exports = app;
