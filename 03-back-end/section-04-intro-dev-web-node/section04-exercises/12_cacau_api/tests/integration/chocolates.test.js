ef45b6ca3cf56b0b517de2606d22bf8ee3679a68
davidrogger <davidrogger.a@gmail.com>
2023-06-13 16:51:14 -0300

    FEAT: get all chocolates test

---
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/integration/chocolates.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+const chocolates = require('../mock/mock_chocolates');
+
+const app = {};
+
+describe('Method Get /chocolates', () => {
+  it('Should list all chocolates', async () => {
+    const response = await chai.request(app).get('/chocolates');
+    expect(response.status).to.be.equals(200);
+    expect(response.body.chocolates).to.be.deep.equal(chocolates.all);
+  });
+});
---
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/mock/mock_chocolates.js
@@ -0,0 +1,10 @@
+const all = [
+  { id: 1, name: 'Mint Intense', brandId: 1 },
+  { id: 2, name: 'White Coconut', brandId: 1 },
+  { id: 3, name: 'Mon Chéri', brandId: 2 },
+  { id: 4, name: 'Mounds', brandId: 3 },
+];
+
+module.exports = {
+  all,
+};
