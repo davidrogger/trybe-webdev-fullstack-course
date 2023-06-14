4a717a08a2dda98aaf42b37066aa01a61f0104b3
davidrogger <davidrogger.a@gmail.com>
2023-06-14 14:08:38 -0300

    CHORE: added stub to the mock file

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/integration/chocolates.test.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/integration/chocolates.test.js
@@ -4,14 +4,23 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const { expect } = chai;
 
-const chocolates = require('../mock/mock_chocolates');
+const sinon = require('sinon');
+const fs = require('fs');
+const { mockFile } = require('../mock/mock_chocolates');
 
 const app = require('../../src/app/app');
 
 describe('Method Get /chocolates', () => {
+  beforeEach(() => {
+    sinon.stub(fs.promises, 'readFile').resolves(JSON.stringify(mockFile));
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+
   it('Should list all chocolates', async () => {
     const response = await chai.request(app).get('/chocolates');
     expect(response.status).to.be.equals(200);
-    expect(response.body.chocolates).to.be.deep.equal(chocolates.all);
+    expect(response.body.chocolates).to.be.deep.equal(mockFile.chocolates);
   });
 });
