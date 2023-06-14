e9e899456e2f6f48120f99f50c5ba33f00530e03
davidrogger <davidrogger.a@gmail.com>
2023-06-14 15:16:38 -0300

    TEST: get chocolate by brand

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/integration/chocolates.test.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/integration/chocolates.test.js
@@ -6,7 +6,7 @@ const { expect } = chai;
 
 const sinon = require('sinon');
 const fs = require('fs');
-const { mockFile } = require('../mock/mock_chocolates');
+const { mockFile, mockChocoByBrand } = require('../mock/mock_chocolates');
 
 const app = require('../../src/app/app');
 
@@ -18,7 +18,7 @@ describe('Route Get /chocolates', () => {
     sinon.restore();
   });
 
-  describe('When calling just chocolates', () => {
+  describe('When calling route /chocolates', () => {
     it('Should list all chocolates', async () => {
       const response = await chai.request(app).get('/chocolates');
       expect(response.status).to.be.equals(200);
@@ -26,11 +26,37 @@ describe('Route Get /chocolates', () => {
     });
   });
 
-  describe('When calling specifing an id', () => {
-    it('Should get information about that chocolate id', async () => {
+  describe('When calling specifing an id, /chocolates/:id', () => {
+    it('Should get information about that chocolate id if found it', async () => {
       const response = await chai.request(app).get('/chocolates/1');
       expect(response.status).to.be.equals(200);
       expect(response.body.chocolate).to.be.deep.equal(mockFile.chocolates[0]);
+    });
+
+    it('Should response 404 if the id is not found', async () => {
+      const response = await chai.request(app).get('/chocolates/10');
+      expect(response.status).to.be.equals(404);
+      expect(response.body.message).to.be.equals('Chocolate ID not found');
+    });
+  });
+
+  describe('When looking for the brand id /chocolates/brand/:id', () => {
+    it('Should list all chocolates from that brand id', async () => {
+      const brands = [1, 2];
+
+      await Promise.all(
+        brands.map(async (brand) => {
+          const endpoint = `/chocolates/brand/${brand}`;
+          const response = await chai.request(app).get(endpoint);
+          expect(response.status).to.be.equals(200);
+          expect(response.body.chocolates).to.be.equals(mockChocoByBrand[brand]);
+        }),
+      );
+    });
+    it('Should response 404 if the brand is not found', async () => {
+      const response = await chai.request(app).get('/chocolates/brand/10');
+      expect(response.status).to.be.equals(404);
+      expect(response.body.message).to.be.equals('Brand ID not found');
     });
   });
 });
--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/mock/mock_chocolates.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/mock/mock_chocolates.js
@@ -37,6 +37,28 @@ const mockFile = {
   ],
 };
 
+const mockChocoByBrand = {
+  1: [
+    {
+      id: 1,
+      name: 'Mint Intense',
+      brandId: 1,
+    },
+    {
+      id: 2,
+      name: 'White Coconut',
+      brandId: 1,
+    }],
+  2: [
+    {
+      id: 3,
+      name: 'Mon Chéri',
+      brandId: 2,
+    },
+  ],
+};
+
 module.exports = {
   mockFile,
+  mockChocoByBrand,
 };
