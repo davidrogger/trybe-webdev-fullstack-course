ac5245caaf98fc688dd8fd9766648c0614dfca16
davidrogger <davidrogger.a@gmail.com>
2023-06-14 14:25:47 -0300

    TEST: get chocolate by id

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/integration/chocolates.test.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/12_cacau_api/tests/integration/chocolates.test.js
@@ -10,7 +10,7 @@ const { mockFile } = require('../mock/mock_chocolates');
 
 const app = require('../../src/app/app');
 
-describe('Method Get /chocolates', () => {
+describe('Route Get /chocolates', () => {
   beforeEach(() => {
     sinon.stub(fs.promises, 'readFile').resolves(JSON.stringify(mockFile));
   });
@@ -18,9 +18,19 @@ describe('Method Get /chocolates', () => {
     sinon.restore();
   });
 
-  it('Should list all chocolates', async () => {
-    const response = await chai.request(app).get('/chocolates');
-    expect(response.status).to.be.equals(200);
-    expect(response.body.chocolates).to.be.deep.equal(mockFile.chocolates);
+  describe('When calling just chocolates', () => {
+    it('Should list all chocolates', async () => {
+      const response = await chai.request(app).get('/chocolates');
+      expect(response.status).to.be.equals(200);
+      expect(response.body.chocolates).to.be.deep.equal(mockFile.chocolates);
+    });
+  });
+
+  describe('When calling specifing an id', () => {
+    it('Should get information about that chocolate id', async () => {
+      const response = await chai.request(app).get('/chocolates/1');
+      expect(response.status).to.be.equals(200);
+      expect(response.body.chocolate).to.be.deep.equal(mockFile.chocolates[0]);
+    });
   });
 });
