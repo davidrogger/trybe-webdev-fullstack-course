6360ea3e48ffd88f12083ea3c247209cd4c5ddc7
davidrogger <davidrogger.a@gmail.com>
2023-06-16 14:55:02 -0300

    TEST: route post to record a new activity sucessfully

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/15_ecoturismo/tests/integration/activities.test.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/15_ecoturismo/tests/integration/activities.test.js
@@ -1,3 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const http = require('../../src/assets/httpStatus');
+const expected = require('../assets/expectedResponses');
+const request = require('../assets/requestsTest');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const app = null; // not implemented yet
+
 describe('Testing route /activites', () => {
-  throw new Error('Not implemented yet');
+  describe('When requesting a new post active successfully', () => {
+    it(
+      'Should respond status 201 with a message "Activity successfully recorded".',
+      async () => {
+        const response = await chai.request(app).post('/activities', request.POST.newActivityBodyTest);
+        expect(response.status).to.be.equal(http.CREATED);
+        expect(response.body.message).to.be.equal(expected.message.createdOK);
+      },
+    );
+  });
+  describe('When requesting a new post active fail', () => {});
 });
