a272bd5d98d9dae38ada9c0202d2088f1ad56702
davidrogger <davidrogger.a@gmail.com>
2023-06-19 14:04:41 -0300

    FEAT: validation price as required field body

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/15_ecoturismo/src/middleware/validation.middlware.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/15_ecoturismo/src/middleware/validation.middlware.js
@@ -3,15 +3,32 @@ const { BadRequestError } = require('../errors/BadRequestError');
 
 const requiredValidations = Router();
 
+function isUndefined(field) {
+  return !field && field !== 0;
+}
+
+function isNotANumber(field) {
+  return Number.isNaN(Number(field));
+}
+
 function verifyName(req, _res, next) {
   const { name } = req.body;
 
-  if (!name) throw new BadRequestError('Name field is required');
+  if (isUndefined(name)) throw new BadRequestError('Name field is required');
   if (name.length < 4) throw new BadRequestError('Name need at least 4 characters');
 
   next();
 }
 
-requiredValidations.use([verifyName]);
+function verifyPrice(req, _res, next) {
+  const { price } = req.body;
+
+  if (isUndefined(price)) throw new BadRequestError('Price field is required');
+  if (isNotANumber(price) || price < 0) throw new BadRequestError('Price need to be a number equal or above 0');
+
+  next();
+}
+
+requiredValidations.use([verifyName, verifyPrice]);
 
 module.exports = requiredValidations;
