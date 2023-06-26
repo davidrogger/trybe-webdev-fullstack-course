53a00def0d9785707872fd7f86f44eba9d359d03
davidrogger <davidrogger.a@gmail.com>
2023-06-26 16:44:16 -0300

    TEST: id not found when updateding and delete a person

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/16_cash/tests/integration/people.test.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/16_cash/tests/integration/people.test.js
@@ -42,7 +42,7 @@ describe('Testing Route /people', () => {
         expect(body).to.be.deep.equal(dataMock.peopleList);
       });
     });
-    describe('Getting a people from its ID', () => {
+    describe('Getting a person with an ID', () => {
       it('Should return status 200, with the ID selected', async () => {
         sinon.stub(connection, 'execute').resolves([[dataMock.peopleList[0]]]);
 
@@ -54,7 +54,7 @@ describe('Testing Route /people', () => {
         expect(body).to.be.deep.equal(dataMock.peopleList[0]);
       });
     });
-    describe('Getting a people that\'s ID don\'t exists', () => {
+    describe('Getting a person information that\'s ID don\'t exists', () => {
       it('Should return status 404 message "ID not found"', async () => {
         sinon.stub(connection, 'execute').resolves([[]]);
 
@@ -68,7 +68,7 @@ describe('Testing Route /people', () => {
     });
   });
 
-  describe('When updating people information', () => {
+  describe('When updating person information', () => {
     it('Should return status 200 with the updated', async () => {
       sinon.stub(connection, 'execute').resolves([{ affectedRows: 1 }]);
 
@@ -79,9 +79,19 @@ describe('Testing Route /people', () => {
       expect(status).to.be.equal(200);
       expect(body).to.be.deep.equal({ id: 1, ...dataMock.updateValidUser });
     });
+    it('Should return status 404 message "ID: xx not found" when not found the id', async () => {
+      sinon.stub(connection, 'execute').resolves([{ affectedRows: 0 }]);
+
+      const { status, body } = await chai
+        .request(app)
+        .put(`${peopleRoute}/2`)
+        .send(dataMock.updateValidUser);
+      expect(status).to.be.equal(404);
+      expect(body).to.be.deep.equal({ message: 'ID: 2 not found' });
+    });
   });
 
-  describe('When deleting a people', () => {
+  describe('When deleting a person', () => {
     it('Should return status 200 message "People ID xx was deleted"', async () => {
       sinon.stub(connection, 'execute').resolves([{ affectedRows: 1 }]);
 
@@ -90,6 +100,15 @@ describe('Testing Route /people', () => {
         .delete(`${peopleRoute}/1`);
       expect(status).to.be.equal(200);
       expect(body.message).to.be.deep.equal('People ID 1 was deleted');
+    });
+    it('Should return status 404 message "ID: xx not found" when not found the id', async () => {
+      sinon.stub(connection, 'execute').resolves([{ affectedRows: 0 }]);
+
+      const { status, body } = await chai
+        .request(app)
+        .delete(`${peopleRoute}/2`);
+      expect(status).to.be.equal(404);
+      expect(body).to.be.deep.equal({ message: 'ID: 2 not found' });
     });
   });
 });
