e3c4c2cd392d614964ca922b54457c192f98e89a
davidrogger <davidrogger.a@gmail.com>
2023-06-26 15:43:10 -0300

    TEST: person id not found

--- 03-back-end/section-04-intro-dev-web-node/section04-exercises/16_cash/tests/integration/people.test.js
+++ 03-back-end/section-04-intro-dev-web-node/section04-exercises/16_cash/tests/integration/people.test.js
@@ -15,7 +15,7 @@ const peopleRoute = '/people';
 describe('Testing Route /people', () => {
   afterEach(sinon.restore);
 
-  describe('When requesting to POST a new person with success', () => {
+  describe('When requesting to POST a new person', () => {
     it('Should respond 201 with a message "New ID xx recorded with success!"', async () => {
       sinon.stub(connection, 'execute').resolves([{ insertId: 33 }]);
 
@@ -29,7 +29,7 @@ describe('Testing Route /people', () => {
     });
   });
 
-  describe('When requesting to GET with success', () => {
+  describe('When requesting to GET', () => {
     describe('Getting all people in the data', () => {
       it('Should return 200 with all people in the database', async () => {
         sinon.stub(connection, 'execute').resolves([dataMock.peopleList]);
@@ -52,6 +52,18 @@ describe('Testing Route /people', () => {
 
         expect(status).to.be.equal(200);
         expect(body).to.be.deep.equal(dataMock.peopleList[0]);
+      });
+    });
+    describe('Getting a people that\'s ID don\'t exists', () => {
+      it('Should return status 404 message "ID not found"', async () => {
+        sinon.stub(connection, 'execute').resolves([[]]);
+
+        const { status, body } = await chai
+          .request(app)
+          .get(`${peopleRoute}/2`);
+
+        expect(status).to.be.equal(404);
+        expect(body.message).to.be.equal('ID not found');
       });
     });
   });
