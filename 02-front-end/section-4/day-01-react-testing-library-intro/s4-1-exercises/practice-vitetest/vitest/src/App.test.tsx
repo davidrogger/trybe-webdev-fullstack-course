86d8988eab233ffbe450dd89ad1e695ad0cd9dd9
davidrogger <davidrogger.a@gmail.com>
2023-07-11 16:16:09 -0300

    TEST: case use, send email

--- 02-front-end/section-4/day-01-react-testing-library-intro/s4-1-exercises/practice-vitetest/vitest/src/App.test.tsx
+++ 02-front-end/section-4/day-01-react-testing-library-intro/s4-1-exercises/practice-vitetest/vitest/src/App.test.tsx
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from './App';
 
 it('Should render label "Email"', () => {
   render(<App />);
-  expect(screen.getByLabelText('Email')).toBeInTheDocument();
+  expect(screen.getByLabelText('Email:')).toBeInTheDocument();
 });
 
 it('Should render button "Enviar"', () => {
@@ -15,4 +16,20 @@ it('Should render button "Enviar"', () => {
 it('Should render button "Voltar"', () => {
   render(<App />);
   expect(screen.getByRole('button', { name: 'Voltar' })).toBeInTheDocument();
+});
+
+it('Should save the email in sided with "Value:" message when clicked in "Enviar"', async () => {
+  render(<App />);
+  const emailInput = screen.getByLabelText('Email:');
+  const sendBtn = screen.getByRole('button', { name: 'Enviar' });
+  const emailSaved = screen.getByRole('heading', { name: 'Valor:' });
+
+  const EMAIL_TYPED = 'teste@teste.com';
+
+  await userEvent.type(emailInput, EMAIL_TYPED);
+  await userEvent.click(sendBtn);
+
+  expect(emailInput).toBeEmptyDOMElement();
+  expect(emailSaved).toHaveTextContent(`Valor: ${EMAIL_TYPED}`);
+  
 });
