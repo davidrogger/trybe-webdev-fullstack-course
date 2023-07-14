d6c795a9a5a96a0c510205cc21388583ac194556
davidrogger <davidrogger.a@gmail.com>
2023-07-14 08:22:08 -0300

    TEST: navigate to about page

--- 02-front-end/section-4/day-03-testing-routes/s4-3-exercises/route-tests/tests/App.test.tsx
+++ 02-front-end/section-4/day-03-testing-routes/s4-3-exercises/route-tests/tests/App.test.tsx
@@ -1,11 +1,23 @@
 // src/App.test.tsx
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from '../src/App';
 import { BrowserRouter } from 'react-router-dom';
 
-it('Renderiza texto da página inicial', () => {
-  render(<App />, { wrapper: BrowserRouter }  );
-  expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
-})
+describe('Testing "App" behavior', () => {
+  it('Should render the right text in the home screen', () => {
+    render(<App />, { wrapper: BrowserRouter }  );
+    expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
+  })
+  
+  it('Should render the right text in the about screen', async () => {
+    render(<App />, { wrapper: BrowserRouter });
+    expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
+  
+    const aboutLink = screen.getByRole('link', { name: /Sobre/i});
+    await userEvent.click(aboutLink);
+    expect(screen.getByText(/Você está na página Sobre/i)).toBeInTheDocument();
+  })
+});
